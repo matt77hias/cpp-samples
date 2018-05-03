514a61c6d01ee010dc2f5f2e7b607ba96d20eec6
Matthias Moulin <matthias.moulin@gmail.com>
2018-05-03 09:27:02 +0200

    Update code.hpp

--- Samples/transform/code.hpp
+++ Samples/transform/code.hpp
@@ -1,20 +1,19 @@
-#include <algorithm>
 #include <array>
 #include <iostream>
-#include <utility>
 
 template< typename ActionT, typename T, size_t...I >
-constexpr decltype(auto) TransformArrayImpl(ActionT action, 
-                                            const std::array< T, sizeof...(I) >& a, 
-                                            std::index_sequence< I... >) {
+constexpr auto TransformArrayImpl(ActionT&& action, 
+                                  const std::array< T, sizeof...(I) >& a, 
+                                  std::index_sequence< I... >) {
     using ToT = decltype(std::declval< ActionT >()(std::declval< T >()));
     return std::array< ToT, sizeof...(I) >{ action(a[I])... };
 }
 
 template< typename ActionT, typename T, size_t N >
-constexpr decltype(auto) TransformArray(ActionT action, 
-                                        const std::array< T, N >& a) {
-    return TransformArrayImpl(action, a, std::make_index_sequence< N >());
+constexpr auto TransformArray(ActionT&& action, 
+                              const std::array< T, N >& a) {
+    return TransformArrayImpl(std::forward< ActionT >(action), a, 
+                              std::make_index_sequence< N >());
 }
 
 template< typename T, std::size_t N >
@@ -25,7 +24,7 @@ std::ostream& operator<<(std::ostream& os, const std::array< T, N >& a) {
 
 int main() {
     constexpr std::array< float, 5 > a = { 1,2,3,4,5 };
-    const auto f = [](float x) { return x*x; };
+    constexpr auto f = [](float x) { return x*x; };
     std::cout << TransformArray(f, a);
     
     return 0;
