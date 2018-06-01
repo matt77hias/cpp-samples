3bc4440b35c7bde4f11f4da3653914b21c120e35
Matthias Moulin <matthias.moulin@gmail.com>
2018-06-01 10:15:58 +0200

    Create code.hpp

---
+++ Samples/clone/code.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct Base {
+    virtual Base* Clone() const {
+        std::cout << "Base::Clone\n";
+        return new Base(*this);
+    }
+};
+
+struct Derived : public Base {
+    virtual Derived* Clone() const override {
+        std::cout << "Derived::Clone\n";
+        return new Derived(*this);
+    }
+};
+
+int main() {
+    Base* b = new Derived;
+    b->Clone();
+    return 0;
+}
