e0694e614d78ea2445bd3811ad7feab203c84312
Matthias Moulin <matthias.moulin@gmail.com>
2018-10-04 21:01:59 +0200

    Update code.hpp

--- Samples/sfinae_type/code.hpp
+++ Samples/sfinae_type/code.hpp
@@ -1,10 +1,15 @@
 #include <type_traits>
 
 template< typename T >
-using Float = std::enable_if_t< std::is_floating_point_v< T >, T >;
+using FloatingPoint = std::enable_if_t< std::is_floating_point_v< T >, T >;
+
+template< typename T >
+using Integral = std::enable_if_t< std::is_integral_v< T >, T >;
 
 int main() {
-    Float< float > f = 1.0f;
-    //Float< int > i = 1; 
+    FloatingPoint< float > f2f = 1.0f;
+    //FloatingPoint< int > i2f = 1; 
+    //Integral< float > f2i = 1.0f;
+    Integral< int > i2i = 1;
     return 0;
 }
