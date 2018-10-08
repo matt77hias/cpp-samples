4fb3e545ddc572f2a3b01c6a7a47f54bb3647198
Matthias Moulin <matthias.moulin@gmail.com>
2018-10-08 11:32:13 +0200

    Update code.hpp

--- Samples/typeid/code.hpp
+++ Samples/typeid/code.hpp
@@ -23,58 +23,58 @@ struct Node {
 	};
 
 	template< typename ComponentT >
-	ComponentT *Get() noexcept {
+	ComponentT* Get() noexcept {
 		const auto it = m_components.find(typeid(ComponentT));
 		return (it != m_components.end()) ? 
-			static_cast< ComponentT * >(it->second) : nullptr;
+			static_cast< ComponentT* >(it->second) : nullptr;
 	}
 
 	template< typename ComponentT >
-	const ComponentT *Get() const noexcept {
+	const ComponentT* Get() const noexcept {
 		const auto it = m_components.find(typeid(ComponentT));
 		return (it != m_components.cend()) ? 
-			static_cast< ComponentT * >(it->second) : nullptr;
+			static_cast< ComponentT* >(it->second) : nullptr;
 
 	}
 
 	template< typename ComponentT >
-	std::vector< ComponentT * > GetAll() {
-		std::vector< ComponentT * > components;
+	std::vector< ComponentT* > GetAll() {
+		std::vector< ComponentT* > components;
 		const auto range = m_components.equal_range(typeid(ComponentT));
 		for_each(range.first, range.second, 
-			[&components](decltype(m_components)::value_type &x) {
-				components.push_back(static_cast< ComponentT * >(x.second));
+			[&components](decltype(m_components)::value_type& x) {
+				components.push_back(static_cast< ComponentT* >(x.second));
 			}
 		);
 		return components;
 	}
 
 	template< typename ComponentT >
-	std::vector< const ComponentT * > GetAll() const {
-		std::vector< ComponentT * > components;
+	std::vector< const ComponentT* > GetAll() const {
+		std::vector< ComponentT* > components;
 		const auto range = m_components.equal_range(typeid(ComponentT));
 		for_each(range.first, range.second,
-			[&components](decltype(m_components)::value_type &x) {
-				components.push_back(static_cast< ComponentT * >(x.second));
+			[&components](decltype(m_components)::value_type& x) {
+				components.push_back(static_cast< ComponentT* >(x.second));
 			}
 		);
 		return components;
 	}
     
-    void AddComponent(Component *component) {
+    void AddComponent(Component* component) {
 		m_components.emplace(typeid(*component), component);
 	}
     
-    std::unordered_multimap< std::type_index, Component * > m_components;
+    std::unordered_multimap< std::type_index, Component* > m_components;
 };
 
 int main() {
     Node n;
-    A *c = new A();
+    A* c = new A();
     n.AddComponent(c);
     
-    A *g = n.Get< A >();
-    std::vector< A * > gs = n.GetAll< A >();
+    A* g = n.Get< A >();
+    std::vector< A* > gs = n.GetAll< A >();
     std::cout << gs.size() << std::endl;
     std::cout << "----" << std::endl;
     std::cout << n.Has< A >() << std::endl;
