3350c6a87d7b98740fa3cec9bee31499778234d6
Matthias Moulin <matthias.moulin@gmail.com>
2018-10-09 21:45:52 +0200

    Update code.hpp

--- Samples/remainder/code.hpp
+++ Samples/remainder/code.hpp
@@ -1,10 +1,20 @@
 #include <cmath>
 #include <iostream>
 
+float Mod(float angle) noexcept {
+    return angle - 360.0f * round(angle / 360.0f);
+}
+
 int main() {
+    std::cout << Mod(-181.0f) << std::endl;
+    std::cout << Mod(-180.0f) << std::endl;
+    std::cout << Mod( 180.0f) << std::endl;
+    std::cout << Mod( 181.0f) << std::endl;
+
     std::cout << std::remainder(-181.0f, 360.0f) << std::endl;
     std::cout << std::remainder(-180.0f, 360.0f) << std::endl;
     std::cout << std::remainder( 180.0f, 360.0f) << std::endl;
     std::cout << std::remainder( 181.0f, 360.0f) << std::endl;
+	
     return 0;
 }
