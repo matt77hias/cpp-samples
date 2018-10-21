11c64876bf7d1d6d2571fe2ea6172e7e95ea4440
Matthias Moulin <matthias.moulin@gmail.com>
2018-10-21 15:17:01 +0200

    Update code.hpp

--- Samples/is_even/code.hpp
+++ Samples/is_even/code.hpp
@@ -1,6 +1,15 @@
+#include <bitset>
+#include <cstdint>
 #include <iostream>
 
-constexpr bool IsEven(unsigned int x) {
+// even integer: 2a   -> (2a)^2   = 4a^2          -> even integer
+//  odd integer: 2a+1 -> (2a+1)^2 = 4a^2 + 4a + 1 -> odd integer
+
+// (2a)^n   = 2^n a^n      = [a^n << n]
+// (2a+1)^n = (2a+1)(2b+1) = 4ab + 2(a+b) + 1 = [ab << 2] + [(a+b) << 1] + 1
+
+bool IsEven(std::uint32_t x) {
+    std::cout << std::bitset< 32 >(x) << std::endl;
 	switch(x) {
 		case 0u: return true;
 		case 1u: return false;
@@ -9,7 +18,14 @@ constexpr bool IsEven(unsigned int x) {
 }
 
 int main() {
-	std::cout << IsEven(3u) << std::endl;
-	std::cout << IsEven(4u) << std::endl;
+    std::cout << std::boolalpha;
+    
+    std::cout << "Is 3 even?"            << std::endl;
+    const auto result3 = IsEven(3u);
+	std::cout << result3 << std::endl;
+    
+    std::cout << "Is 4 even?"            << std::endl;
+    const auto result4 = IsEven(4u);
+	std::cout << result4 << std::endl;
 	return 0;
 }
