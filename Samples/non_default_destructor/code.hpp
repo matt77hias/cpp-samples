e2ce08f5a73b45a42c12e3c155f7da1db694a72d
Matthias Moulin <matthias.moulin@gmail.com>
2018-10-28 09:37:21 +0100

    Create code.hpp

---
+++ Samples/non_default_destructor/code.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+struct A {
+    ~A() { std::cout << "A::~A()" << std::endl; }
+};
+struct B {
+    ~B() { std::cout << "B::~B()" << std::endl; }
+    A m_a;
+};
+
+int main() {
+    B b;
+    return 0;
+}
