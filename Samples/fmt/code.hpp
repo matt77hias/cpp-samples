3d70164669c104939029759ee93e0efe648b50f0
Matthias Moulin <matthias.moulin@gmail.com>
2018-11-17 19:25:35 +0100

    Create code.hpp

---
+++ Samples/fmt/code.hpp
@@ -0,0 +1,117 @@
+#include <fmt/format.h>
+#include <string_view>
+
+struct UTF16toUTF8 {
+
+    explicit UTF16toUTF8([[maybe_unused]] std::wstring_view) {}
+    
+    explicit operator std::string_view() const noexcept {
+        return m_str;
+    }
+
+    std::string m_str = "string";
+};
+
+struct UTF8toUTF16 {
+
+    explicit UTF8toUTF16([[maybe_unused]] std::string_view) {}
+    
+    explicit operator std::wstring_view() const noexcept {
+        return m_str;
+    }
+
+    std::wstring m_str = L"wstring";
+};
+
+namespace fmt {
+
+	template<>
+	struct formatter< std::wstring_view, char > {
+
+		template< typename ParseContextT >
+		typename ParseContextT::iterator parse(ParseContextT& ctx) {
+			return ctx.begin();
+		}
+
+		template< typename FormatContextT >
+		auto format(const std::wstring_view& wstr, FormatContextT& ctx) {
+			const UTF16toUTF8 str(wstr);
+			return format_to(ctx.begin(), "{}", std::string_view(str));
+		}
+	};
+
+	template<>
+	struct formatter< std::wstring, char > {
+
+		template< typename ParseContextT >
+		typename ParseContextT::iterator parse(ParseContextT& ctx) {
+			return ctx.begin();
+		}
+
+		template< typename FormatContextT >
+		auto format(const std::wstring& wstr, FormatContextT& ctx) {
+			const UTF16toUTF8 str(wstr);
+			return format_to(ctx.begin(), "{}", std::string_view(str));
+		}
+	};
+
+	template<>
+	struct formatter< std::string_view, wchar_t > {
+
+		template< typename ParseContextT >
+		typename ParseContextT::iterator parse(ParseContextT& ctx) {
+			return ctx.begin();
+		}
+
+		template< typename FormatContextT >
+		auto format(const std::string_view& str, FormatContextT& ctx) {
+			const UTF8toUTF16 wstr(str);
+			return format_to(ctx.begin(), L"{}", std::wstring_view(wstr));
+		}
+	};
+
+	template<>
+	struct formatter< std::string, wchar_t > {
+
+		template< typename ParseContextT >
+		typename ParseContextT::iterator parse(ParseContextT& ctx) {
+			return ctx.begin();
+		}
+
+		template< typename FormatContextT >
+		auto format(const std::string& str, FormatContextT& ctx) {
+			const UTF8toUTF16 wstr(str);
+			return format_to(ctx.begin(), L"{}", std::wstring_view(wstr));
+		}
+	};
+}
+
+template< typename... ArgsT >
+inline void Print(std::string_view format_str, const ArgsT&... args) {
+    return fmt::print(format_str, args...);
+}
+
+template< typename... ArgsT >
+inline void Print(std::wstring_view format_str, const ArgsT&... args) {
+    return fmt::print(format_str, args...);
+}
+
+int main() {
+    std::string s = "foo";
+    std::string_view sv = s;
+
+    std::string ws = "bar";
+    std::string_view wsv = ws;
+
+    Print( "{}", s);
+    Print( "{}", sv);
+    Print(L"{}", s);
+    Print(L"{}", sv);
+
+    Print( "{}", ws);
+    Print( "{}", wsv);
+    Print(L"{}", ws);
+    Print(L"{}", wsv);
+
+    return 0;
+}
