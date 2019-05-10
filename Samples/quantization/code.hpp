70679dd16dc0b1be3ab4f9539a76836232f3f2f2
Matthias Moulin <matthias.moulin@gmail.com>
2019-05-10 20:40:57 +0200

    Create code.hpp

---
+++ Samples/quantization/code.hpp
@@ -0,0 +1,37 @@
+#include <cstdint>
+#include <iostream>
+
+using F32 = float;
+using U32 = std::uint32_t;
+
+template< std::size_t QuantumSize >
+constexpr F32 QuantizeSNorm(F32 x) {
+    static_assert(QuantumSize <= 31u);
+    const U32 mx = 1u << (QuantumSize - 1u);
+    const F32 nx = std::clamp(x + 0.5f / mx, -1.0f, 1.0f);
+    const U32 ux = U32(((nx >= 0.0) ? mx - 1u : mx) * nx + mx);
+    return (F32(ux) - mx) / ((ux >= mx - 1u) ? mx - 1u : mx);
+}
+
+template< std::size_t QuantumSize >
+constexpr F32 QuantizeUNorm(F32 x) {
+    static_assert(QuantumSize <= 31u);
+    const U32 mx = 1u << QuantumSize;
+    const F32 nx = std::clamp(x + 0.5f / mx, 0.0f, 1.0f);
+    const U32 ux = U32((mx - 1u) * nx);
+    return F32(ux) / (mx - 1u);
+}
+
+int main() {
+    std::cout << QuantizeUNorm< 8u >( 0.0f) << " , ";
+    std::cout << QuantizeUNorm< 8u >( 0.5f) << " , ";
+    std::cout << QuantizeUNorm< 8u >( 1.0f) << std::endl;
+    
+    std::cout << QuantizeSNorm< 8u >(-1.0f) << " , "; 
+    std::cout << QuantizeSNorm< 8u >(-0.5f) << " , ";
+    std::cout << QuantizeSNorm< 8u >( 0.0f) << " , ";
+    std::cout << QuantizeSNorm< 8u >( 0.5f) << " , ";
+    std::cout << QuantizeSNorm< 8u >( 1.0f) << std::endl;
+    
+    return 0;
+}
