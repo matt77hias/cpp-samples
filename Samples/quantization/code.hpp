cde2fc08f5c7b69b850dddf8e1c661d32394813d
Matthias Moulin <matthias.moulin@gmail.com>
2019-05-14 20:45:52 +0200

    Update code.hpp

--- Samples/quantization/code.hpp
+++ Samples/quantization/code.hpp
@@ -6,8 +6,8 @@ using U32 = std::uint32_t;
 
 template< std::size_t QuantumSize >
 constexpr F32 QuantizeSNorm(F32 x) {
-    static_assert(QuantumSize <= 31u);
-    const U32 mx = 1u << (QuantumSize - 1u);
+    static_assert(QuantumSize <= 16u);
+    constexpr U32 mx = 1u << (QuantumSize - 1u);
     const F32 nx = std::clamp(x + 0.5f / mx, -1.0f, 1.0f);
     const U32 ux = U32(((nx >= 0.0) ? mx - 1u : mx) * nx + mx);
     return (F32(ux) - mx) / ((ux >= mx - 1u) ? mx - 1u : mx);
@@ -15,8 +15,8 @@ constexpr F32 QuantizeSNorm(F32 x) {
 
 template< std::size_t QuantumSize >
 constexpr F32 QuantizeUNorm(F32 x) {
-    static_assert(QuantumSize <= 31u);
-    const U32 mx = 1u << QuantumSize;
+    static_assert(QuantumSize <= 16u);
+    constexpr U32 mx = 1u << QuantumSize;
     const F32 nx = std::clamp(x + 0.5f / mx, 0.0f, 1.0f);
     const U32 ux = U32((mx - 1u) * nx);
     return F32(ux) / (mx - 1u);
