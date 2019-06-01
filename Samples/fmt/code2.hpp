deecc3248faae2706eb308e19f72568679bf92e4
Matthias Moulin <matthias.moulin@gmail.com>
2019-06-01 14:42:51 +0200

    Create code2.hpp

---
+++ Samples/fmt/code2.hpp
@@ -0,0 +1,85 @@
+#include <fmt/format.h>
+#include <string_view>
+
+struct UTF16toUTF8 {
+
+    explicit UTF16toUTF8([[maybe_unused]] std::wstring_view) {}
+    
+    explicit operator std::string_view() const noexcept {
+        return m_str;
+    }
+
+    std::string m_str = "string";
+};
+
+struct UTF8toUTF16 {
+
+    explicit UTF8toUTF16([[maybe_unused]] std::string_view) {}
+    
+    explicit operator std::wstring_view() const noexcept {
+        return m_str;
+    }
+
+    std::wstring m_str = L"wstring";
+};
+
+namespace fmt {
+
+	template<>
+	struct formatter< UTF16toUTF8, char > {
+
+		template< typename ParseContextT >
+		typename ParseContextT::iterator parse(ParseContextT& ctx) {
+			return ctx.begin();
+		}
+
+		template< typename FormatContextT >
+		auto format(const UTF16toUTF8& wstr, FormatContextT& ctx) {
+			return format_to(ctx.out(), "{}", std::string_view(wstr));
+		}
+	};
+
+	template<>
+	struct formatter< UTF8toUTF16, wchar_t > {
+
+		template< typename ParseContextT >
+		typename ParseContextT::iterator parse(ParseContextT& ctx) {
+			return ctx.begin();
+		}
+
+		template< typename FormatContextT >
+		auto format(const UTF8toUTF16& str, FormatContextT& ctx) {
+			return format_to(ctx.out(), L"{}", std::wstring_view(str));
+		}
+	};
+}
+
+template< typename... ArgsT >
+inline void Print(std::string_view format_str, const ArgsT&... args) {
+    return fmt::print(format_str, args...);
+}
+
+template< typename... ArgsT >
+inline void Print(std::wstring_view format_str, const ArgsT&... args) {
+    return fmt::print(format_str, args...);
+}
+
+int main() {
+    std::string s = "foo";
+    std::string_view sv = s;
+
+    std::wstring ws = L"bar";
+    std::wstring_view wsv = ws;
+
+    Print( "{}", s);
+    Print( "{}", sv);
+    Print(L"{}", UTF8toUTF16(s));
+    Print(L"{}", UTF8toUTF16(sv));
+
+    Print( "{}", UTF16toUTF8(ws));
+    Print( "{}", UTF16toUTF8(wsv));
+    Print(L"{}", ws);
+    Print(L"{}", wsv);
+
+    return 0;
+}
