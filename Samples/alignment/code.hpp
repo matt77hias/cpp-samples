e67ce8b40f10b5cdef4fc804f54c6e08f039c6f8
Matthias Moulin <matthias.moulin@gmail.com>
2019-06-06 09:54:05 +0200

    Create code.hpp

---
+++ Samples/alignment/code.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+struct A
+{
+    float m_data[3] = {};
+};
+
+int main()
+{
+    std::cout << alignof(A) << std::endl;
+    std::cout <<  sizeof(A) << std::endl;
+    return 0;
+}
