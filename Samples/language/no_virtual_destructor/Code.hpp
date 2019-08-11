554f3cbe9cbb38c3368331884df230192daef06d
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 15:49:27 +0200

    Update and rename Samples/no_virtual_destructor/code.hpp to Samples/language/no_virtual_destructor/Code.hpp

--- Samples/language/no_virtual_destructor/Code.hpp
+++ Samples/language/no_virtual_destructor/Code.hpp
@@ -1,40 +1,64 @@
+// cout, endl
 #include <iostream>
+// size_t
+#include <cstddef>
+// uintptr_t
+#include <cstdint>
+// free, malloc
+#include <cstdlib>
 
 bool g_logging = false;
 
-void* operator new(std::size_t size) {
+[[nodiscard]]
+void* operator new(std::size_t size)
+{
     const auto ptr = std::malloc(size);
-    if (g_logging) {
+    
+    if (g_logging)
+    {
         std::cout << "Allocated: " << (uintptr_t)ptr << std::endl;
     }
+
     return ptr;
 }
-void operator delete(void* ptr) noexcept {
-    std::free(ptr);
-    if (g_logging) {
+
+void operator delete(void* ptr) noexcept
+{
+    if (g_logging)
+    {
        std::cout << "Deallocated: " << (uintptr_t)ptr << std::endl; 
     }
+
+    std::free(ptr);
 }
 
-struct A {
-    ~A() {
+struct A
+{
+    ~A()
+    {
        std::cout << "A::~A()" << std::endl; 
     } 
+
     virtual void f() {}
 };
 
-struct B : A {
-    ~B() {
+struct B : A
+{
+    ~B()
+    {
        std::cout << "B::~B()" << std::endl; 
     }
 };
 
-int main() {
+int main()
+{
     std::cout << sizeof(A) << std::endl;
     std::cout << sizeof(B) << std::endl;
+    
     g_logging = true;
     A* a = new B();
     delete a;
     g_logging = false;
+    
     return 0;
 }
