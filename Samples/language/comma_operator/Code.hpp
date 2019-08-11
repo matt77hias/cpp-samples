ec34ffd14ee79f0c33424faf44877475915c84f9
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 15:36:31 +0200

    Update and rename Samples/komma/code.hpp to Samples/language/comma_operator/Code.hpp

---
+++ Samples/language/comma_operator/Code.hpp
@@ -0,0 +1,22 @@
+// map
+#include <map>
+// pair
+#include <utility>
+
+enum Node : int { A, B };
+using Edge = std::pair< Node, Node >;
+
+[[nodiscard]]
+const Edge operator,(Node p1, Node p2)
+{
+    return { p1, p2 };
+}
+
+int main()
+{ 
+    std::map< Edge, double > lengths;
+    lengths[A, B] = 123.0;
+    lengths[B, A] = 123.0;
+	
+    return 0;
+}
