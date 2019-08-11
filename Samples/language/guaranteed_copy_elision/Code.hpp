da57e64a8fe58095fb72f438c95471a831b06570
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 15:40:59 +0200

    Update and rename Samples/guaranteed_copy_elision/code.hpp to Samples/language/guaranteed_copy_elision/Code.hpp

---
+++ Samples/language/guaranteed_copy_elision/Code.hpp
@@ -0,0 +1,34 @@
+// cout, endl
+#include <iostream>
+
+struct Widget
+{
+    Widget() { std::cout << "Widget::Widget()" << std::endl; };
+    Widget(const Widget&) = delete;
+    Widget(const Widget&&) = delete;
+    ~Widget() { std::cout << "Widget::~Widget()" << std::endl; };
+    Widget &operator=(const Widget&) = delete;
+    Widget &operator=(const Widget&&) = delete;
+};
+
+[[nodiscard]]
+Widget make_widget()
+{
+    std::cout << "f_non_const" << std::endl;
+    return Widget();
+}
+
+[[nodiscard]]
+const Widget make_const_widget()
+{
+    std::cout << "f_const" << std::endl;
+    return Widget();
+}
+
+int main()
+{
+    const Widget w0 = make_widget();
+    const Widget w1 = make_const_widget();
+
+    return 0;
+}
