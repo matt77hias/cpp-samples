7d8a647437d226e4ceade9cf6fb9289ae943bb2b
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 15:05:53 +0200

    Update and rename code.hpp to Code.hpp

---
+++ Samples/std/regex/Code.hpp
@@ -0,0 +1,21 @@
+// copy
+#include <algorithm>
+// cout
+#include <iostream>
+// ostream_iterator
+#include <iterator>
+// regex, sregex_token_iterator
+#include <regex>
+// string
+#include <string>
+
+int main()
+{
+   const std::string text = "Quick \"\"\"\" \"brown fox\".";
+   std::regex re(R"((\"[^\"]*\")|\S+)");
+   std::copy(std::sregex_token_iterator(text.begin(), text.end(), re, 0),
+             std::sregex_token_iterator(),
+             std::ostream_iterator< std::string >(std::cout, "\n"));
+   
+   return 0;
+}
