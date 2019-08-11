ee2018c84a0da0ac82676bf00deaf0ef146e29b3
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 15:44:45 +0200

    Update and rename Samples/partial_template_specialization/code.hpp to Samples/language/partial_template_specialization/Code.hpp

--- Samples/language/partial_template_specialization/Code.hpp
+++ Samples/language/partial_template_specialization/Code.hpp
@@ -1,20 +1,27 @@
+// array
 #include <array>
+// size_t
+#include <cstddef>
+// cout, endl
 #include <iostream>
 
-template< typename T, size_t N, size_t A = alignof(T) >
+template< typename T, std::size_t N, std::size_t A = alignof(T) >
 struct alignas(A) Array : std::array< T, N > {};
 
 template< typename T >
 struct Widget;
 
-template< typename T, size_t N, size_t A >
-struct Widget< Array< T, N, A > > {
-    void print() const {
+template< typename T, std::size_t N, std::size_t A >
+struct Widget< Array< T, N, A > >
+{
+    void print() const
+    {
         std::cout << "Widget< Array< T, N, A > >::print()" << std::endl;
     }
 };
 
-int main() {
+int main()
+{
     Widget< Array< int, 3 > > w;
     w.print();
     
