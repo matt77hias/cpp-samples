b62a3db33d7b25c34d8d0a6051e40fb95da3e3bc
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 16:17:06 +0200

    Update code2.hpp

--- Samples/clone/code2.hpp
+++ Samples/clone/code2.hpp
@@ -1,40 +1,53 @@
+// cout, endl
 #include <iostream>
+// shared_ptr
 #include <memory>
 
-struct Base {
-
+struct Base
+{
     virtual ~Base() = default;
     
-    std::shared_ptr< Base > Clone() const {
-        std::cout << "Base::Clone\n";
+	[[nodiscard]]
+    std::shared_ptr< Base > Clone() const
+    {
+        std::cout << "Base::Clone" << std::endl;
         return CloneImplementation();
     }
 
 private:
 
-    virtual std::shared_ptr< Base > CloneImplementation() const {
-        std::cout << "Base::CloneImplementation\n";
+	[[nodiscard]]
+    virtual std::shared_ptr< Base > CloneImplementation() const
+    {
+        std::cout << "Base::CloneImplementation" << std::endl;
         return std::shared_ptr< Base >(new Base(*this));
     }
 };
-struct Derived : public Base {
 
-    std::shared_ptr< Derived > Clone() const {
-        std::cout << "Derived::Clone\n";
+struct Derived : public Base
+{
+	[[nodiscard]]
+    std::shared_ptr< Derived > Clone() const
+    {
+        std::cout << "Derived::Clone" << std::endl;
         return std::static_pointer_cast< Derived >(CloneImplementation());
     }
 
 private:
 
-    virtual std::shared_ptr< Base > CloneImplementation() const override {
-        std::cout << "Derived::CloneImplementation\n";
+	[[nodiscard]]
+    virtual std::shared_ptr< Base > CloneImplementation() const override
+    {
+        std::cout << "Derived::CloneImplementation" << std::endl;
         return std::shared_ptr< Derived >(new Derived(*this));
     }
 };
 
-int main() {
+int main()
+{
     Base* b = new Derived;
-    b->Clone();
+    std::shared_ptr< Base > clone = b->Clone();
     delete b;
+
     return 0;
 }
