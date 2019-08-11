6ad2250949699ea6dba8782641500a73910294d2
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 14:29:47 +0200

    Update and rename Samples/unless_operator/code.hpp to Samples/UnlessOperator/Code.hpp

--- Samples/UnlessOperator/Code.hpp
+++ Samples/UnlessOperator/Code.hpp
@@ -1,18 +1,21 @@
+// bitset
 #include <bitset>
+// cout, endl
 #include <iostream>
 
-int main() {
+int main()
+{
     constexpr auto a = 27u;
     constexpr auto b = 7u; // versus 0u
     
-    std::bitset< 32 > c1(! b);
-    std::bitset< 32 > c2(!! b);
-    std::bitset< 32 > c3(-!! b);
+    std::bitset< 32u > c1(  !b);
+    std::bitset< 32u > c2( !!b);
+    std::bitset< 32u > c3(-!!b);
     
     std::cout << c1 << std::endl;
     std::cout << c2 << std::endl;
     std::cout << c3 << std::endl;
     
-    std::cout << (a &-!! b) << std::endl;
+    std::cout << (a & -!!b) << std::endl;
     return 0;
 }
