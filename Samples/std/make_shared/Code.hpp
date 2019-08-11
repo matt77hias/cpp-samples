68bc0ffaf58d30ad62bc0debf55df1eca86e55ec
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 14:57:40 +0200

    Update and rename code.hpp to Code.hpp

--- Samples/std/make_shared/Code.hpp
+++ Samples/std/make_shared/Code.hpp
@@ -1,48 +1,77 @@
+// cout, endl
 #include <iostream>
+// size_t
+#include <cstddef>
+// uintptr_t
+#include <cstdint>
+// free, malloc
+#include <cstdlib>
+// make_shared, shared_ptr, weak_ptr
 #include <memory>
 
 bool g_logging = false;
 
-void* operator new(std::size_t size) {
+[[nodiscard]]
+void* operator new(std::size_t size)
+{
     const auto ptr = std::malloc(size);
-    if (g_logging) {
+    
+    if (g_logging)
+    {
         std::cout << "Allocated: " << (uintptr_t)ptr << std::endl;
     }
+
     return ptr;
 }
-void operator delete(void* ptr) noexcept {
-    std::free(ptr);
-    if (g_logging) {
+
+void operator delete(void* ptr) noexcept
+{
+    if (g_logging)
+    {
        std::cout << "Deallocated: " << (uintptr_t)ptr << std::endl; 
     }
+
+    std::free(ptr);
 }
 
-struct Widget {
-    ~Widget() {
+struct Widget
+{
+    ~Widget()
+    {
         std::cout << "Widget::~Widget()" << std::endl;
     }
-    int data;
+    
+    int m_data;
 };
 
-void test(bool use_make_shared) {
+void test(bool use_make_shared)
+{
     std::shared_ptr< Widget > sp;
-    if (use_make_shared) {
+    if (use_make_shared)
+    {
         sp = std::make_shared< Widget >();
-    } else {
+    }
+    else
+    {
         sp = std::shared_ptr< Widget >(new Widget());
     }
+    
     std::weak_ptr< Widget > wp(sp);
     
     sp.reset();
     std::cout << "No std::shared_ptr's anymore." << std::endl;
+    
     wp.reset();
     std::cout << "No std::weak_ptr's anymore." << std::endl;
 }
 
-int main() { 
+int main()
+{ 
     g_logging = true;
     test(true);
+    
     std::cout << std::endl;
+    
     test(false);
     g_logging = false;
     
