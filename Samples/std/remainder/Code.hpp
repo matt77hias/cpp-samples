2470baf817af1f41d2e630ac4c5137dbdd3c924c
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 15:15:38 +0200

    Update and rename code.hpp to Code.hpp

--- Samples/std/remainder/Code.hpp
+++ Samples/std/remainder/Code.hpp
@@ -1,15 +1,22 @@
+// remainder, round
 #include <cmath>
+// cout, endl
 #include <iostream>
 
-float Mod(float angle) noexcept {
-    return angle - 360.0f * round(angle / 360.0f);
+[[nodiscard]]
+float Mod(float angle) noexcept
+{
+    return angle - 360.0f * std::round(angle / 360.0f);
 }
 
-int main() {
+int main()
+{
     std::cout << Mod(-181.0f) << std::endl;
     std::cout << Mod(-180.0f) << std::endl;
     std::cout << Mod( 180.0f) << std::endl;
     std::cout << Mod( 181.0f) << std::endl;
+
+    std::cout << std::endl;
 
     std::cout << std::remainder(-181.0f, 360.0f) << std::endl;
     std::cout << std::remainder(-180.0f, 360.0f) << std::endl;
