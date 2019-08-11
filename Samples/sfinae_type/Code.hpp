e80f8ac09c62a36e64f18129f9186ac660ced303
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 16:00:51 +0200

    Update and rename code.hpp to Code.hpp

--- Samples/sfinae_type/Code.hpp
+++ Samples/sfinae_type/Code.hpp
@@ -1,4 +1,6 @@
+// cout, endl
 #include <iostream>
+// enable_if_t, is_floating_point_v, is_integral_v
 #include <type_traits>
 
 template< typename T >
@@ -8,16 +10,19 @@ template< typename T >
 using Integral = std::enable_if_t< std::is_integral_v< T >, T >;
 
 template< typename T >
-void f(T f, [[maybe_unused]] FloatingPoint< T > c = T(0)) {
+void f(T f, [[maybe_unused]] FloatingPoint< T > c = T(0))
+{
     std::cout << "FloatingPoint: " << f << std::endl;
 }
 
 template< typename T >
-void f(T f, [[maybe_unused]] Integral< T > c = T(0)) {
+void f(T f, [[maybe_unused]] Integral< T > c = T(0))
+{
     std::cout << "Integral: " << f << std::endl;
 }
 
-int main() {
+int main()
+{
     FloatingPoint< float > f2f = 2.0f;
     //FloatingPoint< int > i2f = 2; 
     //Integral< float > f2i = 2.0f;
