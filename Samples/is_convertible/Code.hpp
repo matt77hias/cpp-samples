d6cccb698e447ac25563bb61d8b54b3b7d8dc35b
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 16:44:15 +0200

    Update and rename code.hpp to Code.hpp

--- Samples/is_convertible/Code.hpp
+++ Samples/is_convertible/Code.hpp
@@ -1,19 +1,26 @@
-#include <type_traits>
+// cout, endl
 #include <iostream>
+// false_type, is_convertible_v, remove_const_t, true_type
+#include <type_traits>
+// declval
+#include <utility>
 
 template< typename FromT, typename ToT, typename = ToT >
-struct is_static_castable : public std::false_type {};
+struct is_static_castable 
+    : public std::false_type
+{};
 
 template< typename FromT, typename ToT >
 struct is_static_castable< FromT, ToT, 
-decltype(static_cast< ToT >(std::declval< FromT >())) > 
-    : public std::true_type {};
+decltype(static_cast< std::remove_const_t< ToT > >(std::declval< std::remove_const_t< FromT > >())) > 
+    : public std::true_type
+{};
 
 template< typename FromT, typename ToT >
-constexpr bool is_static_castable_v 
-		= is_static_castable< FromT, ToT >::value;
+constexpr bool is_static_castable_v = is_static_castable< FromT, ToT >::value;
 
-int main() {
+int main()
+{
     std::cout << is_static_castable_v< float, unsigned int >;
     std::cout << is_static_castable_v< float, int >;
     std::cout << is_static_castable_v< unsigned int, float >;
@@ -28,6 +35,11 @@ int main() {
     std::cout << is_static_castable_v< const float, int >;
     std::cout << is_static_castable_v< const unsigned int, float >;
     std::cout << is_static_castable_v< const int, float >;
+
+    std::cout << is_static_castable_v< const float, const unsigned int >;
+    std::cout << is_static_castable_v< const float, const int >;
+    std::cout << is_static_castable_v< const unsigned int, const float >;
+    std::cout << is_static_castable_v< const int, const float >;
     
     std::cout << std::endl;
     
@@ -45,6 +57,11 @@ int main() {
     std::cout << std::is_convertible_v< const float, int >;
     std::cout << std::is_convertible_v< const unsigned int, float >;
     std::cout << std::is_convertible_v< const int, float >;
+
+    std::cout << std::is_convertible_v< const float, const unsigned int >;
+    std::cout << std::is_convertible_v< const float, const int >;
+    std::cout << std::is_convertible_v< const unsigned int, const float >;
+    std::cout << std::is_convertible_v< const int, const float >;
     
     return 0;
 }
