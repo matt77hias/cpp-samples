6e663879b1014bd7fc1e31139e109e73b1355b12
Matthias Moulin <matthias.moulin@gmail.com>
2019-08-11 15:12:17 +0200

    Update and rename code2.hpp to Code2.hpp

---
+++ Samples/std/regex/Code2.hpp
@@ -0,0 +1,22 @@
+// transform
+#include <algorithm>
+// cout
+#include <iostream>
+// ostream_iterator
+#include <iterator>
+// regex, smatch, sregex_iterator
+#include <regex>
+// string
+#include <string>
+
+int main()
+{
+   const std::string text = "Quick \"\"\"\" \"brown fox\".";
+   std::regex re(R"((\"([^\"]*)\")|(\S+))");
+   std::transform(std::sregex_iterator(text.begin(), text.end(), re),
+                  std::sregex_iterator(),
+                  std::ostream_iterator< std::string >(std::cout, "\n"),
+                  [](const std::smatch& m) { return m[2u].length() ? m[2u] : m[3u]; });
+   
+   return 0;
+}
