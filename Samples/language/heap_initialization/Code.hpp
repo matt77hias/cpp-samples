ea292c76d659a3821168e3aff627eb9cc78d44c5
Matthias Moulin <matthias.moulin@gmail.com>
2019-10-17 08:35:21 +0200

    Create Code.hpp

---
+++ Samples/language/heap_initialization/Code.hpp
@@ -0,0 +1,23 @@
+// cout, endl
+#include <iostream>
+
+int main() 
+{
+    const auto x = new int;
+    const auto y = new int();
+    const auto z = new int{};
+
+    const auto a = new int[2];
+    const auto b = new int[2]();
+    const auto c = new int[2]{};
+
+    std::cout << *x << std::endl;
+    std::cout << *y << std::endl;
+    std::cout << *z << std::endl;
+
+    std::cout << a[0u] << ' ' << a[1u] << std::endl;
+    std::cout << b[0u] << ' ' << b[1u] << std::endl;
+    std::cout << c[0u] << ' ' << c[1u] << std::endl;
+    
+    return 0;
+}
