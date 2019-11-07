39ff25df91323c2a3d45023477ac1af2166a756a
Matthias Moulin <matthias.moulin@gmail.com>
2019-11-07 22:40:47 +0100

    Create Code2.hpp

---
+++ Samples/language/operator_new/Code2.hpp
@@ -0,0 +1,78 @@
+// size_t
+#include <cstddef>
+// cout, endl
+#include <iostream>
+// logic_error
+#include <stdexcept>
+
+struct A 
+{};
+
+struct B
+{
+    B() { throw std::logic_error("Called: B::B()"); }
+    B(int i) : m_i(i) {}
+    int m_i = {};
+};
+
+B g_b(0);
+
+[[nodiscard]]
+void* operator new(std::size_t size, A)
+{
+    std::cout << "Called: void* operator new(std::size_t size, A)" << std::endl;
+    std::cout << "Size: " << size << std::endl;
+    return &g_b;
+}
+
+[[nodiscard]]
+void* operator new[](std::size_t size, A)
+{
+    std::cout << "Called: void* operator new[](std::size_t size, A)" << std::endl;
+    std::cout << "Size: " << size << std::endl;
+    return &g_b;
+}
+
+void operator delete(void* ptr) noexcept
+{
+    if (ptr == &g_b) 
+    {
+        std::cout << "Called: delete(void* ptr)" << std::endl;
+    }
+}
+
+/*
+void operator delete(void* ptr, A) noexcept
+{
+    if (ptr == &g_b) 
+    {
+        std::cout << "Called: delete(void* ptr, A)" << std::endl;
+    }   
+}
+*/
+
+void operator delete[](void* ptr) noexcept
+{
+    if (ptr == &g_b) 
+    {
+        std::cout << "Called: delete[](void* ptr)" << std::endl;
+    }
+}
+
+/*
+void operator delete[](void* ptr, A) noexcept
+{
+    if (ptr == &g_b) 
+    {
+        std::cout << "Called: delete[](void* ptr, A)" << std::endl;
+    }
+}
+*/
+
+int main()
+{
+   A a;
+   try { const auto b1 = new (a) B; }    catch (...) {}
+   try { const auto b2 = new (a) B[5]; } catch (...) {}
+   return 0;
+}
