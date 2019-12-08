5b4e73372a870c812a5969bbd4f50ceefc428c93
Matthias Moulin <matthias.moulin@gmail.com>
2019-12-08 17:00:12 +0100

    Create Code.hpp

---
+++ Samples/language/delete/Code.hpp
@@ -0,0 +1,28 @@
+// string
+#include <string>
+// cout, endl
+#include <iostream>
+
+struct Widget
+{
+    Widget(const std::string&)
+    {
+        std::cout << "Widget(const std::string&)" << std::endl;
+    }
+
+    Widget(const std::string&&) = delete;
+    //{
+    //    std::cout << "Widget(const std::string&&)" << std::endl;
+    //}
+};
+
+std::string Create()
+{
+    return "string";
+}
+
+int main()
+{
+    [[maybe_unused]] Widget w(Create());
+    return 0;
+}
