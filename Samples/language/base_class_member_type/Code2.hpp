4445c106d24ef1c67cd0951b715d7077ed05f742
Matthias Moulin <matthias.moulin@gmail.com>
2020-02-09 18:05:44 +0100

    Create Code2.hpp

---
+++ Samples/language/base_class_member_type/Code2.hpp
@@ -0,0 +1,23 @@
+template< typename T >
+struct A
+{
+    using type = T;
+};
+
+template< typename T >
+struct B : A< T >
+{};
+
+template< typename T >
+constexpr void f(typename T::type value)
+{}
+
+int main()
+{
+    A< int >::type a = {};
+    B< int >::type b = {};
+
+    f< A< int > >({});
+    f< B< int > >({});
+    return 0;
+}
