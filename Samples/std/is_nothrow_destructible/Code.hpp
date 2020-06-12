2c0c624968aa8662ad40acf1760045ec594a2dd7
Matthias Moulin <matthias.moulin@gmail.com>
2020-06-12 19:33:08 +0200

    Create Code.hpp

---
+++ Samples/std/is_nothrow_destructible/Code.hpp
@@ -0,0 +1,55 @@
+// cout, endl
+#include <iostream>
+// is_nothrow_destructible_v
+#include <type_traits>
+// declval
+#include <utility>
+
+int FuncA()
+{
+    return 1;
+}
+
+int FuncB() noexcept
+{
+    return 1;
+}
+
+struct A
+{
+    ~A()
+    {
+        FuncA();
+    }
+};
+
+struct B
+{
+    ~B()
+    {
+        FuncB();
+    }
+};
+
+struct C
+{
+    ~C()
+    {
+        throw 10;
+    }
+};
+
+struct D
+{
+    ~D() noexcept(false)
+    {}
+};
+
+int main()
+{
+    std::cout << std::is_nothrow_destructible_v< A > << std::endl;
+    std::cout << std::is_nothrow_destructible_v< B > << std::endl;
+    std::cout << std::is_nothrow_destructible_v< C > << std::endl;
+    std::cout << std::is_nothrow_destructible_v< D > << std::endl;
+    return 0;
+}
