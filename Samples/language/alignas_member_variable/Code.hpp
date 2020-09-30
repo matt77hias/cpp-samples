915be0e1f63d86408fcd9a123e8fc08364ca83de
Matthias Moulin <matthias.moulin@gmail.com>
2020-09-30 11:57:30 +0200

    Create Code.hpp

---
+++ Samples/language/alignas_member_variable/Code.hpp
@@ -0,0 +1,25 @@
+// byte
+#include <cstddef>
+// declval
+#include <utility>
+
+struct A
+{
+    std::byte m_value = {};
+};
+
+static_assert(sizeof(A) == 1u);
+
+struct B
+{
+    alignas(16u) A m_a = {};
+};
+
+static_assert(sizeof(B) == 16u);
+static_assert(sizeof(std::declval< B >().m_a) == 1u);
+static_assert(sizeof(decltype(std::declval< B >().m_a)) == 1u);
+
+int main()
+{
+    return 0;
+}
