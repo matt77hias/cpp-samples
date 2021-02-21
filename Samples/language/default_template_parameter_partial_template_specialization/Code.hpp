d1216cb7838b4aeb71f2ce2e277a17415e460d55
Matthias Moulin <matthias.moulin@gmail.com>
2021-02-21 17:49:51 +0100

    Create Code.hpp

---
+++ Samples/language/default_template_parameter_partial_template_specialization/Code.hpp
@@ -0,0 +1,19 @@
+// size_t
+#include <cstddef>
+
+template< typename T, std::size_t A = alignof(T) >
+struct Alignment
+{
+    static constexpr auto s_alignment = A;
+};
+
+template< typename T, std::size_t A >
+struct Alignment< T[], A >
+{
+    static constexpr auto s_alignment = A;
+};
+
+int main()
+{
+    return static_cast< int >(Alignment< char[] >::s_alignment);
+}
