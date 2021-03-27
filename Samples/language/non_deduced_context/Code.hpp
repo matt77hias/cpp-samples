6dde26493b2e431db08e22eb59ab50a63af40958
Matthias Moulin <matthias.moulin@gmail.com>
2021-03-27 19:14:52 +0100

    Create Code.hpp

---
+++ Samples/language/non_deduced_context/Code.hpp
@@ -0,0 +1,113 @@
+// size_t
+#include <cstddef>
+// same_as
+#include <concepts>
+// tuple_element, tuple_size
+#include <tuple>
+// integral_constant
+#include <type_traits>
+
+template< typename T >
+struct Outer
+{
+    using value_type = T;
+
+    struct Inner
+    {
+        using value_type = typename Outer::value_type;
+
+        int m_first  = {};
+        int m_second = {};
+    };
+};
+
+namespace std
+{
+  template< typename T >
+  requires(std::same_as< T, typename ::Outer< typename T::value_type >::Inner >)
+  class tuple_size< T > : public integral_constant< size_t, 2 >
+  {};
+
+  template< typename T >
+  requires(std::same_as< T, typename ::Outer< typename T::value_type >::Inner >)
+  struct tuple_element< 0u, T >
+  {
+      using type = int;
+  };
+
+  template< typename T >
+  requires(std::same_as< T, typename ::Outer< typename T::value_type >::Inner >)
+  struct tuple_element< 1u, T >
+  {
+      using type = int;
+  };
+}
+
+template< std::size_t I, typename T >
+requires(std::same_as< T, typename Outer< typename T::value_type >::Inner >)
+[[nodiscard]]
+inline std::tuple_element_t< I, T >& get(T& src)
+{
+    if constexpr (I == 0u)
+    {
+        return src.m_first;
+    }
+    if constexpr (I == 1u)
+    {
+        return src.m_second;
+    }
+}
+
+template< std::size_t I, typename T >
+requires(std::same_as< T, typename Outer< typename T::value_type >::Inner >)
+[[nodiscard]]
+inline const std::tuple_element_t< I, T >& get(const T& src)
+{
+    if constexpr (I == 0u)
+    {
+        return src.m_first;
+    }
+    if constexpr (I == 1u)
+    {
+        return src.m_second;
+    }
+}
+
+template< std::size_t I, typename T >
+requires(std::same_as< T, typename Outer< typename T::value_type >::Inner >)
+[[nodiscard]]
+inline std::tuple_element_t< I, T >&& get(T&& src)
+{
+    if constexpr (I == 0u)
+    {
+        return src.m_first;
+    }
+    if constexpr (I == 1u)
+    {
+        return src.m_second;
+    }
+}
+
+template< std::size_t I, typename T >
+requires(std::same_as< T, typename Outer< typename T::value_type >::Inner >)
+[[nodiscard]]
+inline const std::tuple_element_t< I, T >&& get(const T&& src)
+{
+    if constexpr (I == 0u)
+    {
+        return src.m_first;
+    }
+    if constexpr (I == 1u)
+    {
+        return src.m_second;
+    }
+}
+
+int main()
+{
+    Outer< int >::Inner ref;
+    auto& [f, s] = ref;
+    f = 3; s = 5;
+    
+    return ref.m_first + ref.m_second;
+}
