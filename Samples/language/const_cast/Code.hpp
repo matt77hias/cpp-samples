8b62f137fe5332be9d801c853119d4ecd683e5a1
Matthias Moulin <matthias.moulin@gmail.com>
2021-04-26 22:21:31 +0200

    Update Code.hpp

--- Samples/language/const_cast/Code.hpp
+++ Samples/language/const_cast/Code.hpp
@@ -1,3 +1,5 @@
+// Cref.: http://eel.is/c++draft/dcl.type.cv#4
+
 struct Foo
 {
     int& m_i;
@@ -18,31 +20,31 @@ int main()
    // Foo
    {
        const Foo f{ .m_i = i }; // declared const Foo
-       const_cast< Foo& >(f).m_i = 1; // not UB?
+       const_cast< Foo& >(f).m_i = 1; // not UB
    }
    {
        const Foo* f = new const Foo{ .m_i = i }; // declared pointer to const Foo
-       const_cast< Foo* >(f)->m_i = 1; // not UB?
+       const_cast< Foo* >(f)->m_i = 1; // not UB
    }
    
    // Bar
    {
        const Bar b{ .m_i = i }; // declared const Bar
-       const_cast< Bar& >(b).m_i = 1; // UB?
+       const_cast< Bar& >(b).m_i = 1; // UB
    }
    {
        const Bar* b = new const Bar{ .m_i = i }; // declared pointer to const Bar
-       const_cast< Bar* >(b)->m_i = 1; // UB?
+       const_cast< Bar* >(b)->m_i = 1; // UB
    }
    
    // Baz
    {
        const Baz b{ .m_i = i }; // declared const Baz
-       const_cast< Baz& >(b).m_i = 1; // not UB?
+       const_cast< Baz& >(b).m_i = 1; // not UB
    }
    {
        const Baz* b = new const Baz{ .m_i = i }; // declared pointer to const Baz
-       const_cast< Baz* >(b)->m_i = 1; // not UB?
+       const_cast< Baz* >(b)->m_i = 1; // not UB
    }
 
     return i;
