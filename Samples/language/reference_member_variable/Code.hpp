c3699b1d024406a6da1b0eb7264742317b07bfd3
Matthias Moulin <matthias.moulin@gmail.com>
2021-04-27 20:32:38 +0200

    Update Code.hpp

--- Samples/language/reference_member_variable/Code.hpp
+++ Samples/language/reference_member_variable/Code.hpp
@@ -1,12 +1,18 @@
-struct A
+struct Widget
 {
-    int& m_i;
+    int* m_a;
+    int& m_b;
+    int  m_c;
 };
 
-int main()
+int main() 
 {
     int i = 0;
-    const A a = { .m_i = i };
-    a.m_i = 7;
+    const Widget w{ .m_a = &i, .m_b = i, .m_c = i };
+    
+    *w.m_a = 1;
+     w.m_b = 2;
+    //w.m_c = 3;
+
     return i;
 }
