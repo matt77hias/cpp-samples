814de34183d9d8be05e362fbb45f7153400e9b2e
Matthias Moulin <matthias.moulin@gmail.com>
2021-05-14 18:31:46 +0200

    Create Code.hpp

---
+++ Samples/eigen/overloading/Code.hpp
@@ -0,0 +1,30 @@
+// Matrix, StorageOptions
+#include <Eigen/Dense>
+// cout
+#include <iostream>
+
+using F32 = float;
+
+using StaticF32x2 = Eigen::Matrix< F32, 1u, 2u >;
+static_assert(sizeof(StaticF32x2) == 2u * sizeof(F32));
+
+using StaticF32x3 = Eigen::Matrix< F32, 1u, 3u >;
+static_assert(sizeof(StaticF32x3) == 3u * sizeof(F32));
+
+void Foobar(const StaticF32x2& v)
+{
+    std::cout << "Foobar(const StaticF32x2& v)";
+}
+
+void Foobar(const StaticF32x3& v)
+{
+    std::cout << "Foobar(const StaticF32x3& v)";
+}
+
+int main()
+{
+    StaticF32x3 v = {};
+    Foobar(v);
+    
+    return 0;
+}
