fa8d303fec912dd58b41945f3df577fcc8cf9d40
Matthias Moulin <matthias.moulin@gmail.com>
2021-06-15 19:44:37 +0200

    Create Code.hpp

---
+++ Samples/language/forward_declaration_scope/Code.hpp
@@ -0,0 +1,14 @@
+// same_as
+#include <concepts>
+
+template< typename T >
+struct Foo
+{};
+
+template< typename T >
+struct Outer
+{
+    using Inner = Foo< struct Bar >;
+};
+
+static_assert(std::same_as< Outer< int >::Inner, Outer< float >::Inner >);
