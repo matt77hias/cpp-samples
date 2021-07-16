32c11cb3906fdf5458749cd808ca6505112d0402
Matthias Moulin <matthias.moulin@gmail.com>
2021-07-16 19:45:39 +0200

    Create Code.hpp

---
+++ Samples/std/bind/Code.hpp
@@ -0,0 +1,26 @@
+// for_each
+#include <algorithm>
+// _1, bind
+#include <functional>
+// cout, endl
+#include <iostream>
+// vector
+#include <vector>
+
+struct Widget
+{
+    void Print(int value) const noexcept
+    {
+        std::cout << value << std::endl;
+    }
+};
+
+int main()
+{
+	std::vector< int > values = { 1, 2 };
+
+    Widget w;
+    std::ranges::for_each(values, std::bind(&Widget::Print, &w, std::placeholders::_1));
+    
+    return 0;
+}
