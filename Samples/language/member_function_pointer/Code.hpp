8e32e6158e66967e5d2be749a4a44f838129fc6a
Matthias Moulin <matthias.moulin@gmail.com>
2021-07-16 19:41:39 +0200

    Create Code.hpp

---
+++ Samples/language/member_function_pointer/Code.hpp
@@ -0,0 +1,32 @@
+// all_of
+#include <algorithm>
+// bind
+#include <functional>
+// cout, endl
+#include <iostream>
+// vector
+#include <vector>
+
+struct Widget
+{
+    [[nodiscard]]
+    bool IsValid() const noexcept
+    {
+        std::cout << m_value << std::endl;
+        
+        return 0 < m_value;
+    }
+
+    int m_value = {};
+};
+
+int main()
+{
+	std::vector< Widget > widgets;
+    widgets.push_back({ .m_value = 1 });
+    widgets.push_back({ .m_value = 2 });
+
+    const bool valid = std::ranges::all_of(widgets, &Widget::IsValid);
+    
+    return 0;
+}
