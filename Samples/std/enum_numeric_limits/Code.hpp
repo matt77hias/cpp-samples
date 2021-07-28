3348be5d4b543ecba9f533db85786ca40c760bef
Matthias Moulin <matthias.moulin@gmail.com>
2021-07-28 19:57:39 +0200

    Create Code.hpp

---
+++ Samples/std/enum_numeric_limits/Code.hpp
@@ -0,0 +1,16 @@
+// numeric_limits
+#include <limits>
+// remove_cv_t
+#include <type_traits>
+
+template< typename T >
+concept Arithmetic = requires
+{
+	std::numeric_limits< std::remove_cv_t< T > >{};
+};
+
+enum A : int {};
+enum class B : int{};
+
+static_assert(Arithmetic< A >);
+static_assert(Arithmetic< B >);
