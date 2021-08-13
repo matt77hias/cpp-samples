71bda5006d95669719de96ada41083d2e4e69b17
Matthias Moulin <matthias.moulin@gmail.com>
2021-08-13 07:43:33 +0200

    Create Code.hpp

---
+++ Samples/std/ranges_project/Code.hpp
@@ -0,0 +1,31 @@
+// lower_bound
+#include <algorithm>
+// less
+#include <functional>
+// pair
+#include <utility>
+// vector
+#include <vector>
+
+struct Widget
+{
+    [[nodiscard]]
+    constexpr auto operator <=>(const Widget& rhs) const noexcept = default;
+};
+
+int main()
+{
+    using key_type   = Widget;
+    using Container  = std::vector< std::pair< Widget, int > >;
+    using value_type = Container::value_type;
+
+    constexpr auto project = [](const value_type& value) noexcept -> const key_type&
+    {
+        return value.first;
+    };
+
+    Container map;
+    const auto it = std::ranges::lower_bound(map, Widget{}, std::ranges::less{}, project);
+
+    return it != map.end();
+}
