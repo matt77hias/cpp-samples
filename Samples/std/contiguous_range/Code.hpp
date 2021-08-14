12ebf9e3ed89bd064301f3b73df06b9a67e42c4a
Matthias Moulin <matthias.moulin@gmail.com>
2021-08-14 15:00:24 +0200

    Create Code.hpp

---
+++ Samples/std/contiguous_range/Code.hpp
@@ -0,0 +1,15 @@
+// array
+#include <array>
+// contiguous_range
+#include <ranges>
+// vector
+#include <vector>
+
+static_assert(std::ranges::contiguous_range< std::array< int, 5u > >);
+static_assert(std::ranges::contiguous_range< std::vector< int > >);
+static_assert(std::ranges::contiguous_range< std::vector< bool > > == false);
+
+int main()
+{
+    return 0;
+}
