5f185d620e4ce644fda3ab254f5636d4ac429262
Matthias Moulin <matthias.moulin@gmail.com>
2021-09-06 14:22:38 +0200

    Update Code.cpp

--- Samples/language/narrowing/Code.cpp
+++ Samples/language/narrowing/Code.cpp
@@ -1,12 +1,49 @@
-// uint16_t, uint8_t
+// bitset
+#include <bitset>
+// int16_t, int8_t, uint16_t, uint8_t
 #include <cstdint>
+// cout
+#include <iostream>
 
 int main()
 {
-    constexpr std::uint16_t src = 0b11111111'01010101u;
-    constexpr std::uint8_t  dst = 0b01010101u;
+    {
+        std::int16_t src = -3;
+        std::bitset< 16u > src_bits(src);
+        std::cout << src_bits << '\n';
+
+        std::uint16_t dst = src;
+        std::bitset< 16u > dst_bits(dst);
+        std::cout << dst_bits << '\n';
+    }
+    {
+        std::uint16_t src = -3;
+        std::bitset< 16u > src_bits(src);
+        std::cout << src_bits << '\n';
+
+        std::int16_t dst = src;
+        std::bitset< 16u > dst_bits(dst);
+        std::cout << dst_bits << '\n';
+    }
     
-    static_assert(dst == static_cast< std::uint8_t >(src));
-    
+    {
+        std::int16_t src = -3;
+        std::bitset< 16u > src_bits(src);
+        std::cout << src_bits << '\n';
+
+        std::uint8_t dst = src;
+        std::bitset< 8u > dst_bits(dst);
+        std::cout << "........" << dst_bits << '\n';
+    }
+    {
+        std::uint16_t src = -3;
+        std::bitset< 16u > src_bits(src);
+        std::cout << src_bits << '\n';
+
+        std::int8_t dst = src;
+        std::bitset< 8u > dst_bits(dst);
+        std::cout << "........" << dst_bits << '\n';
+    }
+   
     return 0;
 }
