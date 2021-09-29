426f0aaacf7c0d47b00dd275f49a30ddf12c28b0
Matthias Moulin <matthias.moulin@gmail.com>
2021-09-29 13:10:06 +0200

    Create Code.hpp

---
+++ Samples/language/zero_intialization/Code.hpp
@@ -0,0 +1,48 @@
+struct A
+{
+    [[nodiscard]]
+    constexpr auto get() const
+        noexcept -> int
+    { 
+        return m_a;
+    }
+    
+    int m_a = 5;
+};
+
+struct B
+{
+
+public:
+
+    constexpr B() : m_b(7) {}
+
+    [[nodiscard]]
+    constexpr auto get() const
+        noexcept -> int
+    { 
+        return m_b;
+    }
+    
+private:
+
+    int m_b;
+};
+
+struct C
+{
+    A m_a;
+    B m_b;
+};
+
+auto main()
+    -> int
+{
+    constexpr C default_init;
+    constexpr C zero_init{};
+
+    static_assert(default_init.m_a.get() + default_init.m_b.get() == 12);
+    static_assert(zero_init.m_a.get()    + zero_init.m_b.get()    == 12);
+
+    return 0;
+}
