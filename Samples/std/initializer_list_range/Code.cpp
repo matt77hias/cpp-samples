3546ea2b09bcde6b9809a51a84ee96f3b1eabab9
Matthias Moulin <matthias.moulin@gmail.com>
2021-11-08 15:31:54 +0100

    Create Code.cpp

---
+++ Samples/std/initializer_list_range/Code.cpp
@@ -0,0 +1,37 @@
+// initializer_list
+#include <initializer_list>
+// cout
+#include <iostream>
+// range
+#include <ranges>
+
+template< std::ranges::range R >
+constexpr void test(R&& range)
+    noexcept
+{
+    std::cout << "1";
+}
+
+constexpr void test(std::initializer_list< int > range)
+    noexcept
+{
+    std::cout << "0";
+    // test< const std::initializer_list< int >& >(range);
+}
+
+auto main()
+  -> int
+{
+    std::initializer_list< int > il = { 1, 2, 3, 4 };
+    auto& ref = il;
+    const std::initializer_list< int > cil = { 1, 2, 3, 4 };
+    const auto& cref = cil;
+
+    test({ 1, 2, 3, 4 });
+    test(il);
+    test(ref);
+    test(cil);
+    test(cref);
+    
+    return 0;
+}
