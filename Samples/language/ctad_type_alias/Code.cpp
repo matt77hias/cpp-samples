c5d4c2417a8df608ce9b05fed91ec958945288e0
Matthias Moulin <matthias.moulin@gmail.com>
2022-01-21 21:30:55 +0100

    Create Code.cpp

---
+++ Samples/language/ctad_type_alias/Code.cpp
@@ -0,0 +1,16 @@
+// size_t
+#include <cstddef>
+// dynamic_extent, span
+#include <span>
+
+template< typename T, std::size_t N = std::dynamic_extent >
+using Span = std::span< T, N >;
+
+auto main() -> int
+{
+    int array[2u] = {};
+
+    Span span = array;
+    
+    return 0;
+}
