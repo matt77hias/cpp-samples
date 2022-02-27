1b5743131220b6be308485044c04bf0509b610ee
Matthias Moulin <matthias.moulin@gmail.com>
2022-02-27 20:53:25 +0100

    Create Code.cpp

---
+++ Samples/language/static_class_member/Code.cpp
@@ -0,0 +1,23 @@
+struct Base
+{
+    [[nodiscard]]
+    static consteval auto f()
+        noexcept -> bool
+    {
+        return true;
+    }
+};
+
+struct Derived : Base
+{};
+
+template< typename T >
+concept Concept = T::f();
+
+static_assert(Derived::f());
+static_assert(Concept< Derived >);
+
+auto main() -> int
+{
+    return 0;
+}
