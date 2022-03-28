bed46ac17c76e78401999bf00a74a509f1abfba5
Matthias Moulin <matthias.moulin@gmail.com>
2022-03-28 12:54:00 +0200

    Create Code.cpp

---
+++ Samples/language/if-consteval/Code.cpp
@@ -0,0 +1,33 @@
+// assert
+#include <cassert>
+// abort
+#include <cstdlib>
+
+[[nodiscard]]
+constexpr auto test(bool value)
+    noexcept -> bool
+{
+    if consteval
+    {
+        // Compile-time assert
+        if (!value)
+        {
+            std::abort(); // Something to break compilation.
+        }
+    }
+    else
+    {
+        // Runtime assert
+        assert(value);
+    }
+
+    return value;
+};
+
+auto main() -> int
+{
+    constexpr auto t1 = test(true);
+    //constexpr auto t2 = test(false);
+
+    return 0;
+}
