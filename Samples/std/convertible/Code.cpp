089ea36848029376f9a93d39971c70f43b76c01a
Matthias Moulin <matthias.moulin@gmail.com>
2022-04-06 19:39:23 +0200

    Create Code.cpp

---
+++ Samples/std/convertible/Code.cpp
@@ -0,0 +1,23 @@
+// is_constructible_v, is_convertible_v
+#include <type_traits>
+
+struct Foo
+{};
+
+struct Bar
+{
+    [[nodiscard]]
+    constexpr operator Foo()
+        noexcept
+    {
+        return {};
+    }
+};
+
+static_assert(std::is_constructible_v< Foo, Bar >);
+static_assert(std::is_convertible_v< Bar, Foo >);
+
+auto main() -> int
+{
+    return 0;
+}
