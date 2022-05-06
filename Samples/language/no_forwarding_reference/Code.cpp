66707f27f36ba0fd813e91e8bf980fca35eb1d4a
Matthias Moulin <matthias.moulin@gmail.com>
2022-05-06 14:49:36 +0200

    Create Code.cpp

---
+++ Samples/language/no_forwarding_reference/Code.cpp
@@ -0,0 +1,23 @@
+// movable
+#include <concepts>
+// is_nothrow_move_assignable_v
+#include <type_traits>
+// move
+#include <utility>
+
+auto main() -> int
+{
+    int dst_value;
+    int src_value = 5;
+    
+    constexpr auto assign = []< std::movable V >(V& dst_value, V&& src_value)
+                            noexcept(std::is_nothrow_move_assignable_v< V >)
+    {
+        dst_value = std::move(src_value);
+    };
+
+    //assign(dst_value, src_value);
+    assign(dst_value, std::move(src_value));
+    
+    return dst_value;
+}
