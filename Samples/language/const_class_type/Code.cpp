ef7e82fd9b46e0c125454b6c2cd8ff800b786169
Matthias Moulin <matthias.moulin@gmail.com>
2022-05-16 18:45:35 +0200

    Create Code.cpp

---
+++ Samples/language/const_class_type/Code.cpp
@@ -0,0 +1,14 @@
+struct Widget
+{
+    static int value;
+};
+
+int Widget::value = 5;
+
+auto main() -> int
+{
+    using type = const Widget;
+    type::value = 7;
+    
+    return type::value;
+}
