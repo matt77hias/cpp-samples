28cc2eaf4051594fef567ea783730927ba4c9d1a
Matthias Moulin <matthias.moulin@gmail.com>
2022-05-16 18:43:28 +0200

    Create Code.cpp

---
+++ Samples/std/unreachable/Code.cpp
@@ -0,0 +1,21 @@
+// unreachable
+#include <utility>
+
+[[nodiscard]]
+auto test(bool flag) noexcept -> int
+{
+    if (true)
+    {
+        return 5;
+    }
+    else
+    {
+        std::unreachable();
+        return {};
+    }
+}
+
+auto main() -> int
+{
+    return test(true);
+}
