f5eb0d5ae5066196f7c9d5c6ea58dafa3230626a
Matthias Moulin <matthias.moulin@gmail.com>
2022-07-17 19:03:37 +0200

    Create Code2.cpp

---
+++ Samples/std/derived_from/Code2.cpp
@@ -0,0 +1,20 @@
+// derived_from
+#include <concepts>
+
+struct A
+{};
+
+struct B : private A
+{
+    friend struct C;
+};
+
+struct C
+{
+    static_assert(not std::derived_from< B, A >);
+};
+
+auto main() -> int
+{
+    return 0;
+}
