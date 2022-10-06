68bff224561de00eee05e02af6e43a664fd3e770
Matthias Moulin <matthias.moulin@gmail.com>
2022-10-06 20:33:45 +0200

    Create Code.hpp

---
+++ Samples/language/shadowing/Code.hpp
@@ -0,0 +1,30 @@
+// cout
+#include <iostream>
+
+template< typename T  >
+void test()
+{
+    std::cout << "test< T >()";
+}
+
+namespace inner
+{
+    template< std::floating_point F >
+    void test()
+    {
+        std::cout << "inner::test< F >()";
+    }
+
+    void main()
+    {
+        test< float >();
+        // test< int >(); shawdowed
+    }
+}
+
+auto main() -> int
+{
+    inner::main();
+
+    return 0;
+}
