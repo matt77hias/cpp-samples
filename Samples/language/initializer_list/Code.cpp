11c6a46a452913e0a51704c2671287fb1db648a4
Matthias Moulin <matthias.moulin@gmail.com>
2023-03-19 21:06:15 +0100

    Create

---
+++ Samples/language/initializer_list/Code.cpp
@@ -0,0 +1,19 @@
+// array
+#include <array>
+// size_t
+#include <cstddef>
+
+template< typename T, std::size_t N  >
+struct Vector
+{
+    Vector(const std::array< T, N >&)
+    {}
+};
+
+auto main() -> int
+{
+    [[maybe_unused]] Vector v1 = std::array{ 1, 2, 3 };
+    [[maybe_unused]] Vector< int, 3u > v2 = { { 1, 2, 3 } };
+
+    return 0;
+}
