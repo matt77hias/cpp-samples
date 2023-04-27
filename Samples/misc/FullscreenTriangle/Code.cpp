d089b76adce47da8b6b19b72826b00de759ed850
Matthias Moulin <matthias.moulin@gmail.com>
2023-04-27 21:53:24 +0200

    Update Code.cpp

--- Samples/misc/FullscreenTriangle/Code.cpp
+++ Samples/misc/FullscreenTriangle/Code.cpp
@@ -4,21 +4,25 @@
 using uint = unsigned int;
 
 [[nodiscard]]
-constexpr float getX(uint i) noexcept
+constexpr auto getX(uint i)
+	noexcept -> float
 {
     return ((i & 2u) << 1u) - 1.0f;
 }
 
 [[nodiscard]]
-constexpr float getY(uint i) noexcept
+constexpr auto getY(uint i)
+	noexcept -> float
 {
     return ((i & 1u) << 2u) - 1.0f;
 }
 
-int main()
+auto main() -> int
 {
     for (uint i = 0u; i < 3u; ++i)
     {
         std::cout << getX(i) << "," << getY(i) << std::endl;
     }
+
+	return 0;
 }
