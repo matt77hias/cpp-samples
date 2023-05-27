ca9c78301895b50e2395ac77ba21ec80f5ac34cd
Matthias Moulin <matthias.moulin@gmail.com>
2023-05-27 20:10:17 +0200

    Update Code.cpp

--- Samples/misc/packing/norm/Code.cpp
+++ Samples/misc/packing/norm/Code.cpp
@@ -11,45 +11,52 @@ using U8  = std::uint8_t;
 using U32 = std::uint32_t;
 
 [[nodiscard]]
-constexpr S8 F32ToSNorm(F32 x) noexcept
+constexpr auto F32ToSNorm(F32 x)
+	noexcept -> S8
 {
     const auto nx = std::clamp(x, -1.0f, 1.0f);
     return S8(((nx >= 0.0f) ? 127.0f : 128.0f) * nx);
 }
 
 [[nodiscard]]
-constexpr U8 F32ToUNorm(F32 x) noexcept
+constexpr auto F32ToUNorm(F32 x)
+	noexcept -> U8
 {
     const auto nx = std::clamp(x, 0.0f, 1.0f);
     return U8(255.0f * nx);
 }
 
 [[nodiscard]]
-constexpr U8 F32ToSUNorm(F32 x) noexcept
+constexpr auto F32ToSUNorm(F32 x)
+	noexcept -> U8
 {
     const auto nx = std::clamp(x, -1.0f, 1.0f);
     return U8(128.0f + ((nx >= 0) ? 127.0f : 128.0f) * nx);
 }
 
 [[nodiscard]]
-constexpr F32 SNormToF32(S8 x) noexcept
+constexpr auto SNormToF32(S8 x)
+	noexcept -> F32
 {
     return F32(x) / ((x >= 0) ? 127.0f : 128.0f);
 }
 
 [[nodiscard]]
-constexpr F32 UNormToF32(U8 x) noexcept
+constexpr auto UNormToF32(U8 x)
+	noexcept -> F32
 {
     return F32(x) / 255.0f;
 }
 
 [[nodiscard]]
-constexpr F32 SUNormToF32(U8 x) noexcept
+constexpr auto SUNormToF32(U8 x)
+	noexcept -> F32
 {
     return (F32(x) - 128.0f) / ((x >= 127u) ? 127.0f : 128.0f);
 }
 
-int main()
+auto main()
+	-> int
 { 
     std::cout << (int) F32ToSNorm(-1.0f) << " , " << (int) F32ToSNorm(1.0f) << std::endl;
     std::cout << (int) F32ToUNorm( 0.0f) << " , " << (int) F32ToUNorm(1.0f) << std::endl;
