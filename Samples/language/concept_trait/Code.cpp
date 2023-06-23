bc395bcfbc0195f5a513e7fc31348d52ba2d52cb
Matthias Moulin <matthias.moulin@gmail.com>
2023-06-23 16:39:59 +0200

    Create Code.cpp

---
+++ Samples/language/concept_trait/Code.cpp
@@ -0,0 +1,37 @@
+template< typename T >
+struct Adapter
+{
+    using Container = T;
+};
+
+template< typename T >
+inline constexpr bool IsAdapter = false;
+template< typename T >
+inline constexpr bool IsAdapter< Adapter< T > > = true;
+
+template< typename T >
+struct Widget
+{};
+
+template< typename T >
+inline constexpr bool IsWidget = false;
+template< typename T >
+inline constexpr bool IsWidget< Widget< T > > = true;
+
+template< typename T >
+using WidgetAdapter = Adapter< Widget< T > >;
+
+template< typename T >
+//inline constexpr bool IsWidgetAdapter = IsAdapter< T > and IsWidget< typename T::Container >;
+concept IsWidgetAdapter = IsAdapter< T > and IsWidget< typename T::Container >;
+
+static_assert(IsWidgetAdapter< WidgetAdapter< int > >);
+static_assert(not IsWidgetAdapter< int >);
+
+auto main()
+    -> int
+{
+    constexpr auto _ = IsWidgetAdapter< int >;
+    
+    return 0;
+}
