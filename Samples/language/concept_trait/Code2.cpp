8321851359a151410351aa6c6a42a72a20def138
Matthias Moulin <matthias.moulin@gmail.com>
2023-06-23 20:33:29 +0200

    Create Code2.cpp

---
+++ Samples/language/concept_trait/Code2.cpp
@@ -0,0 +1,17 @@
+// random_access_range
+#include <ranges>
+
+template< std::ranges::random_access_range T >
+inline constexpr bool IsVector = false;
+
+template< typename T >
+concept Vector = IsVector< T >;
+
+//static_assert(not IsVector< int >);
+static_assert(not Vector< int >);
+
+auto main()
+    -> int
+{
+    return 0;
+}
