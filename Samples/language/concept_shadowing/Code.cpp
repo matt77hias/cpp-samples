09b95211ca093d650daa58802d7dc9f5495ae1a0
Matthias Moulin <matthias.moulin@gmail.com>
2023-06-23 20:51:07 +0200

    Create Code.cpp

---
+++ Samples/language/concept_shadowing/Code.cpp
@@ -0,0 +1,30 @@
+// integral
+#include <concepts>
+
+template< typename T >
+concept Scalar = true;
+
+template< typename T >
+struct Vector
+{
+    using Scalar = T;
+
+    constexpr auto operator +=(const Scalar& rhs)
+        noexcept -> Vector
+        requires(std::integral< Scalar >);
+};
+
+template< typename T >
+constexpr auto Vector< T >
+    ::operator +=(const Scalar& rhs)
+    noexcept -> Vector
+    requires(std::integral< Scalar >)
+{
+    return *this;
+}
+
+auto main()
+    -> int
+{
+    return 0;
+}
