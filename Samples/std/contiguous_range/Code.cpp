04e6fcb5309cea2e35a98f0aa145c4b496a67f82
Matthias Moulin <matthias.moulin@gmail.com>
2023-07-28 20:06:09 +0200

    Update Code.cpp

--- Samples/std/contiguous_range/Code.cpp
+++ Samples/std/contiguous_range/Code.cpp
@@ -9,7 +9,8 @@ static_assert(std::ranges::contiguous_range< std::array< int, 5u > >);
 static_assert(std::ranges::contiguous_range< std::vector< int > >);
 static_assert(not std::ranges::contiguous_range< std::vector< bool > >);
 
-auto main() -> int
+auto main()
+    -> int
 {
     return 0;
 }
