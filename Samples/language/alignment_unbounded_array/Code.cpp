15bea4224b972fd440a368e2b6231ad508e918f2
Matthias Moulin <matthias.moulin@gmail.com>
2023-09-13 19:53:45 +0200

    Update Code.cpp

--- Samples/language/alignment_unbounded_array/Code.cpp
+++ Samples/language/alignment_unbounded_array/Code.cpp
@@ -1,11 +1,11 @@
 inline constexpr auto Alignment = 4u;
 
-struct alignas(Alignment) A
+struct alignas(Alignment) Widget
 {};
 
-static_assert(alignof(A)     == Alignment);
-static_assert(alignof(A[2u]) == Alignment);
-static_assert(alignof(A[])   == Alignment);
+static_assert(alignof(Widget)     == Alignment);
+static_assert(alignof(Widget[2u]) == Alignment);
+static_assert(alignof(Widget[])   == Alignment);
 
 auto main()
     -> int
