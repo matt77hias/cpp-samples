94cd158a910725f73dc050363c5dc9afa20f5ac4
Matthias Moulin <matthias.moulin@gmail.com>
2023-09-13 19:52:14 +0200

    Update Code.cpp

--- Samples/language/alignment_unbounded_array/Code.cpp
+++ Samples/language/alignment_unbounded_array/Code.cpp
@@ -1,8 +1,12 @@
 struct alignas(4u) A
 {};
 
+static_assert(alignof(A)     == 4u);
+static_assert(alignof(A[2u]) == 4u);
+static_assert(alignof(A[])   == 4u);
+
 auto main()
     -> int
 {
-    return static_cast< int >(alignof(A[]));
+    return 0;
 }
