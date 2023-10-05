70eccf05180849a0a89093c2f3cf3220e5b3284b
Matthias Moulin <matthias.moulin@gmail.com>
2023-10-05 20:59:46 +0200

    Update Code.cpp

--- Samples/language/fold_default/Code.cpp
+++ Samples/language/fold_default/Code.cpp
@@ -2,12 +2,12 @@ template< bool... Bs >
 struct Pack
 {
     // X and true == X
-    static_assert((Bs && ...) == true);
-    static_assert((... && Bs) == true);
+    static_assert((Bs and ...) == true);
+    static_assert((... and Bs) == true);
 
     // X or false == X
-    static_assert((Bs || ...) == false);
-    static_assert((... || Bs) == false);
+    static_assert((Bs or ...) == false);
+    static_assert((... or Bs) == false);
 };
 
 auto main()
