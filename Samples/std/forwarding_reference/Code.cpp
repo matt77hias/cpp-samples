536e458d7a5c6b61107a1cf167c2617599fec4ae
Matthias Moulin <matthias.moulin@gmail.com>
2023-12-17 11:14:52 +0100

    Create Code.cpp

---
+++ Samples/std/forwarding_reference/Code.cpp
@@ -0,0 +1,87 @@
+// constructible_from
+#include <concepts>
+// is_lvalue_reference_v, is_rvalue_reference_v
+#include <type_traits>
+// declval
+#include <type_traits>
+
+template< typename E >
+struct A
+{
+    static void f(E&);
+    static void f(E&&) = delete;
+
+    template< typename T >
+    A(T&& src)
+        noexcept
+        requires(std::convertible_to< T, E& > and requires { f(std::declval< T >()); })
+    {}
+};
+
+template< typename E >
+struct B
+{
+    template< typename T >
+    B(T&& src)
+        noexcept
+        requires(std::convertible_to< T, E& > and not std::is_rvalue_reference_v< decltype(src) >)
+    {}
+};
+
+template< typename E >
+struct C
+{
+    template< typename T >
+    C(T&& src)
+        noexcept
+        requires(std::convertible_to< T, E& > and std::is_lvalue_reference_v< decltype(src) >)
+    {}
+};
+
+auto main()
+    -> int
+{
+    static_assert(not std::constructible_from< A< int >, int >);
+    static_assert(not std::constructible_from< A< int >, const int >);
+    static_assert(std::constructible_from< A< int >, int& >);
+    static_assert(not std::constructible_from< A< int >, const int& >);
+    static_assert(not std::constructible_from< A< int >, int&& >);
+    static_assert(not std::constructible_from< A< int >, const int&& >);
+
+    static_assert(not std::constructible_from< A< const int >, int >);
+    static_assert(not std::constructible_from< A< const int >, const int >);
+    static_assert(std::constructible_from< A< const int >, int& >);
+    static_assert(std::constructible_from< A< const int >, const int& >);
+    static_assert(not std::constructible_from< A< const int >, int&& >);
+    static_assert(not std::constructible_from< A< const int >, const int&& >);
+
+    static_assert(not std::constructible_from< B< int >, int >);
+    static_assert(not std::constructible_from< B< int >, const int >);
+    static_assert(std::constructible_from< B< int >, int& >);
+    static_assert(not std::constructible_from< B< int >, const int& >);
+    static_assert(not std::constructible_from< B< int >, int&& >);
+    static_assert(not std::constructible_from< B< int >, const int&& >);
+
+    static_assert(not std::constructible_from< B< const int >, int >);
+    static_assert(not std::constructible_from< B< const int >, const int >);
+    static_assert(std::constructible_from< B< const int >, int& >);
+    static_assert(std::constructible_from< B< const int >, const int& >);
+    static_assert(not std::constructible_from< B< const int >, int&& >);
+    static_assert(not std::constructible_from< B< const int >, const int&& >);
+
+    static_assert(not std::constructible_from< C< int >, int >);
+    static_assert(not std::constructible_from< C< int >, const int >);
+    static_assert(std::constructible_from< C< int >, int& >);
+    static_assert(not std::constructible_from< C< int >, const int& >);
+    static_assert(not std::constructible_from< C< int >, int&& >);
+    static_assert(not std::constructible_from< C< int >, const int&& >);
+
+    static_assert(not std::constructible_from< C< const int >, int >);
+    static_assert(not std::constructible_from< C< const int >, const int >);
+    static_assert(std::constructible_from< C< const int >, int& >);
+    static_assert(std::constructible_from< C< const int >, const int& >);
+    static_assert(not std::constructible_from< C< const int >, int&& >);
+    static_assert(not std::constructible_from< C< const int >, const int&& >);
+    
+    return 0;
+}
