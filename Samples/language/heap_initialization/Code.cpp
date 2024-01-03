a34b60e3a4a528fa29defa21c431b1c0836d3e3b
Matthias Moulin <matthias.moulin@gmail.com>
2024-01-03 21:00:32 +0100

    Update Code.cpp

--- Samples/language/heap_initialization/Code.cpp
+++ Samples/language/heap_initialization/Code.cpp
@@ -8,17 +8,17 @@ auto main()
     const auto y = new int();
     const auto z = new int{};
 
-    const auto a = new int[2];
-    const auto b = new int[2]();
-    const auto c = new int[2]{};
+    const auto a = new int[2u];
+    const auto b = new int[2u]();
+    const auto c = new int[2u]{};
 
     std::cout << *x << std::endl;
     std::cout << *y << std::endl;
     std::cout << *z << std::endl;
 
-    std::cout << a[0u] << ' ' << a[1u] << std::endl;
-    std::cout << b[0u] << ' ' << b[1u] << std::endl;
-    std::cout << c[0u] << ' ' << c[1u] << std::endl;
+    std::cout << a[0] << ' ' << a[1] << std::endl;
+    std::cout << b[0] << ' ' << b[1] << std::endl;
+    std::cout << c[0] << ' ' << c[1] << std::endl;
     
     return 0;
 }
