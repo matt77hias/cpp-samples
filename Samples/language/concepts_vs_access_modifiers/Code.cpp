0208277fe94cc9f00656bd0a037e0bf33744cf21
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-01 19:49:37 +0200

    Create Code.cpp

---
+++ Samples/language/concepts_vs_access_modifiers/Code.cpp
@@ -0,0 +1,40 @@
+// same_as
+#include <concepts>
+
+template< typename T >
+concept Check = requires(const T t)
+{ 
+   { t.Get() } noexcept -> std::same_as< int >;
+};
+
+struct Foo
+{
+
+public:
+    auto Get() const noexcept -> int;
+};
+
+struct Bar
+{
+
+protected:
+    auto Get() const noexcept -> int;
+};
+
+struct Baz
+{
+
+private:
+    auto Get() const noexcept -> int;
+};
+
+static_assert(Check< Foo >);
+static_assert(not Check< Bar >);
+static_assert(not Check< Baz >);
+
+
+auto main()
+    -> int
+{
+    return 0;
+}
