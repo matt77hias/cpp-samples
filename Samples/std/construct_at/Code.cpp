847f9b195ac5f8fce63f948cc9ad6cab29511504
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-24 18:25:18 +0200

    Create Code.cpp

---
+++ Samples/std/construct_at/Code.cpp
@@ -0,0 +1,20 @@
+// construct_at
+#include <memory>
+
+[[nodiscard]]
+constexpr auto f()
+    -> int
+{
+    int i = {};
+    const int* ip = &i;
+    std::ranges::construct_at(ip, 5);
+    return i;
+}
+
+auto main()
+	noexcept -> int
+{
+    constexpr auto result = f();
+    
+    return result;
+}
