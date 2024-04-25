b824abce66126403ef50871edf457bff35b33171
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-25 22:20:08 +0200

    Create Code2.cpp

---
+++ Samples/std/initializer_list/Code2.cpp
@@ -0,0 +1,37 @@
+// same_as
+#include <concepts>
+// initializer_list
+#include <initializer_list>
+// cout, endl
+#include <iostream>
+
+struct A
+{
+    A()
+    {
+        std::cout << "default" << std::endl;
+    }
+    
+    template< typename T = int >
+    A(std::initializer_list< T >)
+    {
+        if constexpr (std::same_as< T, int >)
+        {
+            std::cout << "initializer_list int" << std::endl;
+        }
+        else
+        {
+            std::cout << "initializer_list" << std::endl;
+        }
+    }
+};
+
+auto main()
+	noexcept -> int
+{
+    [[maybe_unused]] A a0 = {};
+    [[maybe_unused]]A a1 = { 1.0f };
+    [[maybe_unused]]A a2 = { {} };
+
+    return 0;
+}
