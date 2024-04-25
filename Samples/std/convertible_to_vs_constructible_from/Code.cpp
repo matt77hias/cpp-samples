34394f11bdb01f830ba50bf98e47cd4d32127523
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-25 11:41:43 +0200

    Update Code.cpp

--- Samples/std/convertible_to_vs_constructible_from/Code.cpp
+++ Samples/std/convertible_to_vs_constructible_from/Code.cpp
@@ -26,23 +26,23 @@ struct B
     }
 };
 
+static_assert(std::constructible_from< A, Foo >);
+static_assert(std::constructible_from< A, Bar >);
+static_assert(not std::convertible_to< Foo, A >);
+static_assert(std::convertible_to< Bar, A >);
+
+static_assert(std::constructible_from< Foo, B >);
+static_assert(std::constructible_from< Bar, B >);
+static_assert(not std::convertible_to< B, Foo >);
+static_assert(std::convertible_to< B, Bar >);
+
+static_assert(std::constructible_from< A, B >);
+static_assert(std::constructible_from< A, B >);
+static_assert(not std::convertible_to< B, A >);
+static_assert(not std::convertible_to< B, A >);
+
 auto main()
 	noexcept -> int
 {
-    static_assert(std::constructible_from< A, Foo >);
-    static_assert(std::constructible_from< A, Bar >);
-    static_assert(not std::convertible_to< Foo, A >);
-    static_assert(std::convertible_to< Bar, A >);
-
-    static_assert(std::constructible_from< Foo, B >);
-    static_assert(std::constructible_from< Bar, B >);
-    static_assert(not std::convertible_to< B, Foo >);
-    static_assert(std::convertible_to< B, Bar >);
-
-    static_assert(std::constructible_from< A, B >);
-    static_assert(std::constructible_from< A, B >);
-    static_assert(not std::convertible_to< B, A >);
-    static_assert(not std::convertible_to< B, A >);
-
     return 0;
 }
