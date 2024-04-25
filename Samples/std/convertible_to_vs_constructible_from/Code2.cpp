4fef4a44bd3b145146218f5cc97d61f94f48a5fb
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-25 11:42:07 +0200

    Update Code2.cpp

--- Samples/std/convertible_to_vs_constructible_from/Code2.cpp
+++ Samples/std/convertible_to_vs_constructible_from/Code2.cpp
@@ -26,23 +26,23 @@ struct B
     }
 };
 
+static_assert(std::is_constructible_v< A, Foo >);
+static_assert(std::is_constructible_v< A, Bar >);
+static_assert(not std::is_convertible_v< Foo, A >);
+static_assert(std::is_convertible_v< Bar, A >);
+
+static_assert(std::is_constructible_v< Foo, B >);
+static_assert(std::is_constructible_v< Bar, B >);
+static_assert(not std::is_convertible_v< B, Foo >);
+static_assert(std::is_convertible_v< B, Bar >);
+
+static_assert(std::is_constructible_v< A, B >);
+static_assert(std::is_constructible_v< A, B >);
+static_assert(not std::is_convertible_v< B, A >);
+static_assert(not std::is_convertible_v< B, A >);
+
 auto main()
 	noexcept -> int
 {
-    static_assert(std::is_constructible_v< A, Foo >);
-    static_assert(std::is_constructible_v< A, Bar >);
-    static_assert(not std::is_convertible_v< Foo, A >);
-    static_assert(std::is_convertible_v< Bar, A >);
-
-    static_assert(std::is_constructible_v< Foo, B >);
-    static_assert(std::is_constructible_v< Bar, B >);
-    static_assert(not std::is_convertible_v< B, Foo >);
-    static_assert(std::is_convertible_v< B, Bar >);
-
-    static_assert(std::is_constructible_v< A, B >);
-    static_assert(std::is_constructible_v< A, B >);
-    static_assert(not std::is_convertible_v< B, A >);
-    static_assert(not std::is_convertible_v< B, A >);
-
     return 0;
 }
