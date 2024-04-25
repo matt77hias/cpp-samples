3c4a82f8992f3ba6094f9f54e541d36169655b53
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-25 11:33:56 +0200

    Create Code.cpp

---
+++ Samples/std/initializer_list_convertible_to/Code.cpp
@@ -0,0 +1,50 @@
+// constructible_from, convertible_to
+#include <concepts>
+// initializer_list
+#include <initializer_list>
+// cout, endl
+#include <iostream>
+// string
+#include <string>
+// vector
+#include <vector>
+
+struct String
+{
+    String(const char* str)
+    {
+        std::cout << str << std::endl;
+    }
+    String(const String&)
+    {
+        std::cout << "copy" << std::endl;
+    }
+};
+
+template< typename T >
+struct Vector
+{
+    template< std::convertible_to< T > SrcT >
+    Vector(std::initializer_list< SrcT >)
+    {
+        std::cout << "initializer_list" << std::endl;
+    }
+};
+
+auto main()
+	noexcept -> int
+{
+    [[maybe_unused]] std::vector< std::string > v1 = { "Foo", "Bar", "Baz" };
+    static_assert(std::constructible_from< std::vector< std::string >, std::initializer_list< std::string > >);
+    static_assert(not std::constructible_from< std::vector< std::string >, std::initializer_list< const char* > >);
+
+    [[maybe_unused]] std::vector< String > v2 = { "Foo", "Bar", "Baz" };
+    static_assert(std::constructible_from< std::vector< String >, std::initializer_list< String > >);
+    static_assert(not std::constructible_from< std::vector< String >, std::initializer_list< const char* > >);
+
+    [[maybe_unused]] Vector< String > v3 = { "Foo", "Bar", "Baz" };
+    static_assert(std::constructible_from< Vector< String >, std::initializer_list< String > >);
+    static_assert(std::constructible_from< Vector< String >, std::initializer_list< const char* > >);
+    
+    return 0;
+}
