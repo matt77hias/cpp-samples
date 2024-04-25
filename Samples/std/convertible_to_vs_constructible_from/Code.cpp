ecd102d33d03e13d4e1649d1db25bed7d3db0492
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-25 11:39:24 +0200

    Create Code.cpp

---
+++ Samples/std/convertible_to_vs_constructible_from/Code.cpp
@@ -0,0 +1,48 @@
+// constructible_from, convertible_to
+#include <concepts>
+
+struct Foo
+{};
+struct Bar
+{};
+
+struct A
+{
+    explicit A(Foo)
+    {}
+    A(Bar)
+    {}
+};
+
+struct B
+{
+    explicit operator Foo() const
+    {
+        return {};
+    }
+    operator Bar() const
+    {
+        return {};
+    }
+};
+
+auto main()
+	noexcept -> int
+{
+    static_assert(std::constructible_from< A, Foo >);
+    static_assert(std::constructible_from< A, Bar >);
+    static_assert(not std::convertible_to< Foo, A >);
+    static_assert(std::convertible_to< Bar, A >);
+
+    static_assert(std::constructible_from< Foo, B >);
+    static_assert(std::constructible_from< Bar, B >);
+    static_assert(not std::convertible_to< B, Foo >);
+    static_assert(std::convertible_to< B, Bar >);
+
+    static_assert(std::constructible_from< A, B >);
+    static_assert(std::constructible_from< A, B >);
+    static_assert(not std::convertible_to< B, A >);
+    static_assert(not std::convertible_to< B, A >);
+
+    return 0;
+}
