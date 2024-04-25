f4c941b0c240663216623167cf1b733fb929ca43
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-25 11:41:04 +0200

    Create Code2.cpp

---
+++ Samples/std/convertible_to_vs_constructible_from/Code2.cpp
@@ -0,0 +1,48 @@
+// is_constructible_v, is_convertible_v
+#include <type_traits>
+
+struct Foo
+{};
+struct Bar
+{};
+
+struct A
+{
+    explicit A(Foo)
+    {}
+    A(Bar)
+    {}
+};
+
+struct B
+{
+    explicit operator Foo() const
+    {
+        return {};
+    }
+    operator Bar() const
+    {
+        return {};
+    }
+};
+
+auto main()
+	noexcept -> int
+{
+    static_assert(std::is_constructible_v< A, Foo >);
+    static_assert(std::is_constructible_v< A, Bar >);
+    static_assert(not std::is_convertible_v< Foo, A >);
+    static_assert(std::is_convertible_v< Bar, A >);
+
+    static_assert(std::is_constructible_v< Foo, B >);
+    static_assert(std::is_constructible_v< Bar, B >);
+    static_assert(not std::is_convertible_v< B, Foo >);
+    static_assert(std::is_convertible_v< B, Bar >);
+
+    static_assert(std::is_constructible_v< A, B >);
+    static_assert(std::is_constructible_v< A, B >);
+    static_assert(not std::is_convertible_v< B, A >);
+    static_assert(not std::is_convertible_v< B, A >);
+
+    return 0;
+}
