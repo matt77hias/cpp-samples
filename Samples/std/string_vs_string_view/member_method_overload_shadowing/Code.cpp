e0d523d666fbbeaa2d6870b2bf3df2edb9d31e38
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-26 12:02:03 +0200

    Create Code.cpp

---
+++ Samples/std/string_vs_string_view/member_method_overload_shadowing/Code.cpp
@@ -0,0 +1,33 @@
+// cout, endl
+#include <iostream>
+
+struct A
+{
+    virtual void f(int) = 0;
+    template< typename T >
+    void f()
+    {
+        std::cout << "f<T>()" << std::endl;
+        f(0);
+    }
+};
+
+struct B : A
+{
+    using A::f;
+    
+    void f(int) override
+    {
+        std::cout << "f(int)" << std::endl;
+    }
+};
+
+auto main()
+	noexcept -> int
+{
+    B b;
+    b.f(5);
+    b.f< int >();
+
+    return 0;
+}
