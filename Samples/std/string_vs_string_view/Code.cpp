499d98a9ca3e2044f33b977fa8a1f319d51424a2
Matthias Moulin <matthias.moulin@gmail.com>
2024-04-26 11:14:28 +0200

    Create Code.cpp

---
+++ Samples/std/string_vs_string_view/Code.cpp
@@ -0,0 +1,16 @@
+// string
+#include <string>
+// string_view
+#include <string_view>
+
+static_assert(not std::convertible_to< std::string_view, std::string >);
+
+auto main()
+	noexcept -> int
+{
+    [[maybe_unused]] std::string src = "";
+    [[maybe_unused]] std::string_view src_view = src;
+    [[maybe_unused]] std::string dst{ src_view };
+
+    return 0;
+}
