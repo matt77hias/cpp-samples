efd63bdd6136b16c214db18850f5b781c4c3a316
Matthias Moulin <matthias.moulin@gmail.com>
2024-05-03 20:32:49 +0200

    Create Code.cpp

---
+++ Samples/language/dynamic_cast_vs_private_inheritance/Code.cpp
@@ -0,0 +1,38 @@
+// cout, endl
+#include <iostream>
+
+struct Base
+{
+    virtual ~Base() = default;
+};
+
+struct Derived : private Base
+{
+    auto GetBase()
+        -> Base*
+    {
+        return this;
+    }
+    
+    static void test()
+    {
+        Derived d;
+        Base* base = d.GetBase();
+
+        if (dynamic_cast< Derived* >(base))
+        {
+            std::cout << "It worked (we must be using public inheritance)." << std::endl;
+        }
+        else
+        {
+            std::cout << "It failed (we must be using protected/private inheritance)." << std::endl;
+        }
+    }
+};
+
+auto main()
+    -> int
+{
+    Derived::test();
+    return 0;
+}
