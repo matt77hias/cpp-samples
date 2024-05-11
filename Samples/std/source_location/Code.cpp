8cae68d441c552e87c11a28a20038f4288d5a165
Matthias Moulin <matthias.moulin@gmail.com>
2024-05-11 20:41:46 +0200

    Create Code.cpp

---
+++ Samples/std/source_location/Code.cpp
@@ -0,0 +1,35 @@
+// cout, endl
+#include <iostream>
+// source_location
+#include <source_location>
+
+struct Diagnostics1
+{
+    std::source_location m_source_location = {};
+};
+
+auto f1(Diagnostics1 d = { .m_source_location = std::source_location::current() })
+{
+    std::cout << d.m_source_location.function_name() << std::endl;
+    std::cout << d.m_source_location.line() << std::endl;
+}
+
+struct Diagnostics2
+{
+    std::source_location m_source_location = std::source_location::current();
+};
+
+auto f2(Diagnostics2 d = {})
+{
+    std::cout << d.m_source_location.function_name() << std::endl;
+    std::cout << d.m_source_location.line() << std::endl;
+}
+
+auto main()
+    -> int
+{
+    f1();
+    f2();
+
+    return 0;
+}
