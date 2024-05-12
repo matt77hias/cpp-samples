a1d3700f1dc6de702347c96d00878991c626fa01
Matthias Moulin <matthias.moulin@gmail.com>
2024-05-12 19:04:07 +0200

    Create Code.cpp

---
+++ Samples/language/private_inheritance_implicit_conversion/Code.cpp
@@ -0,0 +1,44 @@
+template< typename T >
+struct Span
+{
+    using element_type = T;
+};
+
+template< typename C >
+struct StringView : private Span< const C >
+{
+    // StringViews always have an infinite extent
+    // E.g., std::span::first< std::size_t > won't work.
+    
+    using Container = Span< const C >;
+
+    using typename Container::element_type;
+};
+
+template< typename C >
+struct NullTerminatedStringView : private StringView< C >
+{
+    // NullTerminatedStringView always have an implicit null-terminating character
+    // E.g., std::basic_string_view::remove_suffix won't work.
+
+    using Container = StringView< C >;
+
+    using typename Container::element_type;
+
+    [[nodiscard]]
+    constexpr operator Container() const // std::basic_string_view has an explicit range constructor
+        noexcept
+    {
+        return {};
+    }
+};
+
+auto main()
+    -> int
+{
+    NullTerminatedStringView< char > s;
+    //auto sv0 = static_cast< StringView< char > >(s);
+    auto sv1 = s.operator StringView< char >();
+    
+    return 0;
+}
