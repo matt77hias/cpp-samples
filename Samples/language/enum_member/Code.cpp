dd4c5a3ecb4a509f82227f790b339fc6c06a82c4
Matthias Moulin <matthias.moulin@gmail.com>
2024-08-15 20:46:01 +0200

    Create Code.cpp

---
+++ Samples/language/enum_member/Code.cpp
@@ -0,0 +1,54 @@
+struct A
+{
+    enum ScopedEnum
+    {
+        Huey, Dewey, Louie
+    };
+
+    enum UnscopedEnum
+    {
+        UnscopedEnum_Huey,
+        UnscopedEnum_Dewey,
+        UnscopedEnum_Louie
+    };
+};
+
+struct B
+{
+    using ScopedEnum = A::ScopedEnum;
+
+    using UnscopedEnum = A::UnscopedEnum;
+};
+
+constexpr auto a = B::ScopedEnum::Huey;
+//constexpr auto b = B::UnscopedEnum_Huey;
+
+struct C
+{
+    using ScopedEnum = A::ScopedEnum;
+
+    using UnscopedEnum = A::UnscopedEnum;
+    
+    static constexpr auto UnscopedEnum_Huey = A::UnscopedEnum_Huey;
+    static constexpr auto UnscopedEnum_Dewey = A::UnscopedEnum_Dewey;
+    static constexpr auto UnscopedEnum_Louie = A::UnscopedEnum_Louie;
+};
+
+constexpr auto c = C::ScopedEnum::Huey;
+constexpr auto d = C::UnscopedEnum_Huey;
+
+struct D
+{
+    using ScopedEnum = A::ScopedEnum;
+
+    using enum A::UnscopedEnum; // C++20
+};
+
+constexpr auto e = D::ScopedEnum::Huey;
+constexpr auto f = D::UnscopedEnum_Huey;
+
+auto main()
+	noexcept -> int
+{
+    return 0;
+}
