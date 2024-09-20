7f578886a22cc6a44e4cd92726ab1a8bd4a235e7
Matthias Moulin <matthias.moulin@gmail.com>
2024-09-20 20:36:17 +0200

    Update Code.cpp

--- Samples/std/default_initializable/Code.cpp
+++ Samples/std/default_initializable/Code.cpp
@@ -5,6 +5,7 @@
 
 static_assert(std::default_initializable< int >);
 static_assert(not std::default_initializable< const int >);
+
 static_assert(std::is_default_constructible_v< int >);
 static_assert(std::is_default_constructible_v< const int >);
 
@@ -16,6 +17,7 @@ struct Foo
 
 static_assert(std::default_initializable< Foo >);
 static_assert(std::default_initializable< const Foo >);
+
 static_assert(std::is_default_constructible_v< Foo >);
 static_assert(std::is_default_constructible_v< const Foo >);
 
