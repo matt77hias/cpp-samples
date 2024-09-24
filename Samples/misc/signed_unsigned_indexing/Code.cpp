e138ba097fb5ac597f1209f17c41ee8dd02fd31b
Matthias Moulin <matthias.moulin@gmail.com>
2024-09-24 21:11:30 +0200

    Create Code.cpp

---
+++ Samples/misc/signed_unsigned_indexing/Code.cpp
@@ -0,0 +1,28 @@
+// signed_integral
+#include <concepts>
+// size_t
+#include <cstddef>
+
+struct Vector
+{
+    constexpr auto operator [](std::size_t index) const
+        -> int
+    {
+        return 3;
+    }
+
+    template< std::signed_integral S >
+    constexpr auto operator [](S index) const
+        -> int
+    {
+        return 5;
+    }
+};
+
+auto main()
+    -> int
+{
+    const Vector v;
+    
+    return v[0];
+}
