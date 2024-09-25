76c69fdd1b1293b354bcf088feccfa878be93dc1
Matthias Moulin <matthias.moulin@gmail.com>
2024-09-25 21:16:12 +0200

    Update Code.cpp

--- Samples/std/unscoped_enum_vs_integer/Code.cpp
+++ Samples/std/unscoped_enum_vs_integer/Code.cpp
@@ -1,7 +1,11 @@
 // is_arithmetic_v, is_integral_v, is_fundamental_v
 #include <type_traits>
 
-enum UnscopedEnum
+static_assert(std::is_arithmetic_v< int >);
+static_assert(std::is_integral_v< int >);
+static_assert(std::is_fundamental_v< int >);
+
+enum UnscopedEnum : int
 {};
 
 static_assert(not std::is_arithmetic_v< UnscopedEnum >);
