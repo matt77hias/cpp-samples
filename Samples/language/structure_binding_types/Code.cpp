6037597d65c461802b326ce28754eab8413055ab
Matthias Moulin <matthias.moulin@gmail.com>
2024-10-02 20:09:52 +0200

    Create Code.cpp

---
+++ Samples/language/structure_binding_types/Code.cpp
@@ -0,0 +1,52 @@
+// same_as
+#include <concepts>
+// tuple
+#include <tuple>
+
+auto main()
+    -> int
+{
+    int a = {};
+    std::tuple< int, const int, int&, const int& > t = { a, a, a, a };
+    
+    {
+        auto [t0, t1, t2, t3] = t;
+        static_assert(std::same_as< decltype(t0), int >);
+        static_assert(std::same_as< decltype(t1), const int >);
+        static_assert(std::same_as< decltype(t2), int& >);
+        static_assert(std::same_as< decltype(t3), const int& >);
+    }
+    {
+        const auto [t0, t1, t2, t3] = t;
+        static_assert(std::same_as< decltype(t0), const int >);
+        static_assert(std::same_as< decltype(t1), const int >);
+        static_assert(std::same_as< decltype(t2), int& >);
+        static_assert(std::same_as< decltype(t3), const int& >);
+    }
+    {
+        auto& [t0, t1, t2, t3] = t;
+        static_assert(std::same_as< decltype(t0), int >);
+        static_assert(std::same_as< decltype(t1), const int >);
+        static_assert(std::same_as< decltype(t2), int& >);
+        static_assert(std::same_as< decltype(t3), const int& >);
+    }
+    {
+        const auto& [t0, t1, t2, t3] = t;
+        static_assert(std::same_as< decltype(t0), const int >);
+        static_assert(std::same_as< decltype(t1), const int >);
+        static_assert(std::same_as< decltype(t2), int& >);
+        static_assert(std::same_as< decltype(t3), const int& >);
+    }
+    {
+        auto&& [t0, t1, t2, t3] = t;
+        static_assert(std::same_as< decltype(t0), int >);
+        static_assert(std::same_as< decltype(t1), const int >);
+        static_assert(std::same_as< decltype(t2), int& >);
+        static_assert(std::same_as< decltype(t3), const int& >);
+    }
+    {
+        //const auto&& [t0, t1, t2, t3] = t;
+    }
+    
+    return 0;
+}
